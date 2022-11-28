03e94088d5ae9716ec50f2908eecb796ee393748
cfdxkk <b2567240058@gmail.com>
2022-11-28 22:10:38 +0800

    update gitignore :rotating_light:

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'D:/KIRAKIRA/testnuxt/nuxt-app/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///D:/KIRAKIRA/testnuxt/nuxt-app/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
