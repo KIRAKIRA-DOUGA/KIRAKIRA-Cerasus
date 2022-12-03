2dba40486485382f333389489ccd6cd52c1a510d
otomad <56647156+otomad@users.noreply.github.com>
2022-12-03 15:29:15 +0800

    Add PageController

---
+++ components/PageController.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+	import { computed, onMounted } from "vue";
+
+	const props = withDefaults(defineProps<{
+		pages: number;
+		current?: number;
+		displayPageCount: number;
+	}>(), {
+		current: 1,
+	});
+
+	const actualPages = computed(() => Math.min(props.pages, props.displayPageCount));
+
+	const _emits = defineEmits<{
+		(_event: "onChangePage", _eventArg: { page: number }): void;
+	}>();
+
+	onMounted(() => {
+		if (props.pages < 1 || props.displayPageCount < 5 || props.current < 1 || props.current > props.pages)
+			throw new RangeError("参数错误");
+	});
+</script>
+
+<template>
+	<div class="page">
+		<div class="track">
+			<div v-for="i in actualPages" :key="`item-${i}`" @click="$emit('onChangePage', { page: i })">
+				{{ i }}
+			</div>
+		</div>
+		<div class="thumb">
+			{{ current }}
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+	@import "@/styles/theme.scss";
+	@import "@/styles/ease.scss";
+	$size: 36px;
+	.track {
+		background-color: $light-mode-controls-inner-color;
+		box-shadow: inset 0 4px 4px #0000000a;
+		border-radius: 4px;
+		display: flex;
+		width: fit-content;
+		> * {
+			width: $size;
+			height: $size;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			color: $light-mode-icon-color;
+			flex-shrink: 0;
+			cursor: pointer;
+
+			&:hover {
+				background: radial-gradient(50% 250% at 50% 50%, #e9e9e9 33.33%, #e9e9e900 100%);
+			}
+
+			&:active {
+				background: radial-gradient(50% 250% at 50% 50%, #cccccc 33.33%, #cccccc00 100%);
+			}
+		}
+	}
+	.page {
+		position: relative;
+		user-select: none;
+	}
+	.thumb {
+		position: absolute;
+		top: 0;
+		left: calc((v-bind(current) - 1) * $size);
+		width: $size;
+		height: $size;
+		background: $brand-pink-50;
+		box-shadow: 0 2px 4px #f06e8e99;
+		border-radius: 4px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		cursor: text;
+		transition: all $ease-out-max 600ms, left $ease-in-out-max 600ms;
+
+		&:hover {
+			background: $brand-pink-30;
+			box-shadow: 0 9px 9px #f06e8e4d;
+		}
+
+		&:active {
+			background: $brand-pink-70;
+			transform: scale(calc(35 / 36));
+		}
+	}
+</style>
--- pages/HomePage.vue
+++ pages/HomePage.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+	const page = ref(1);
+</script>
+
 <template>
 	<div class="kirakira-home-page-box">
 		<div>
@@ -6,10 +10,11 @@
 		<NuxtLink to="/VideoPlay">
 			video
 		</NuxtLink>
+		<PageController :pages="7" :displayPageCount="7" :current="page" @onChangePage="e => page = e.page" />
 	</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 	.kirakira-home-page-box {
 		background-color: yellow;
 	}
