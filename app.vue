9ca6a12dca9c07043343415e295d20ed5c5d83f2
otomad <56647156+otomad@users.noreply.github.com>
2022-12-05 18:05:40 +0800

    Update head

--- app.vue
+++ app.vue
@@ -19,6 +19,19 @@
 	// 	layout.value = "pocket-edition-web";
 	// 	slotName.value = "pocket-edition-web-slot";
 	// }, 5000);
+
+	useHead({
+		htmlAttrs: {
+			lang: "zh-cmn-Hans-CN", // 中文-普通话-简体字-大陆地区
+		},
+		bodyAttrs: {
+			id: "root",
+		},
+		titleTemplate: "%s - KiRAKiRA☆DOUGA",
+		link: [
+			{ rel: "icon", type: "image/vnd.microsoft.icon", href: "/favicon.ico" }, // 不是 image/x-icon
+		],
+	});
 </script>
 
 <style lang="scss">
