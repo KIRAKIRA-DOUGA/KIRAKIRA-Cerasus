9832df67052666982dd07fc6dbe969a2d3ceb55a
otomad <56647156+otomad@users.noreply.github.com>
2022-12-06 21:25:35 +0800

    Update element

--- composables/element.ts
+++ composables/element.ts
@@ -1,4 +1,10 @@
-export function getPath(target: unknown): Element[] {
+/**
+ * 获取从指定元素节点，一直追溯到根元素的数组。
+ * 用于查找事件目标并向上冒泡，找到需要的元素。
+ * @param target - HTML DOM 节点。
+ * @returns 从指定元素节点，一直追溯到根元素的数组。
+ */
+export function getPath(target: Node | Element | null): Element[] {
 	if (!(target instanceof Element)) return [];
 	const path: Element[] = [];
 	while (target instanceof Element) {
