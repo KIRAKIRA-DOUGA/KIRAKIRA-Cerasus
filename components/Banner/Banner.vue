4c02292e041fed4072d5c2580c3a6f7e49e6b605
otomad <56647156+otomad@users.noreply.github.com>
2022-12-07 22:46:46 +0800

    Update Banner.vue

--- components/Banner/Banner.vue
+++ components/Banner/Banner.vue
@@ -4,11 +4,10 @@
 	</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 	.kirakira-main-page-banner-box {
-
-		/* please romove when deploy  下方CSS请在部署前移除 */
-		background-color: rgba(0, 128, 0, 0.1);
+		// please romove when deploy 下方CSS请在部署前移除
+		background-color: #0080001a;
 
 		width: 100%;
 		height: var(--kirakira-main-page-banner-height);
