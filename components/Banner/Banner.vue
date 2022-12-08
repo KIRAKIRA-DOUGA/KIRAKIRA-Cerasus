5f844dac0d85f34d7231bdaad471d0095fb3db21
otomad <56647156+otomad@users.noreply.github.com>
2022-12-08 19:35:02 +0800

    Drawer banner

--- components/Banner/Banner.vue
+++ components/Banner/Banner.vue
@@ -9,6 +9,7 @@
 <template>
 	<div class="kirakira-main-page-banner-box" :class="{ collapsed }">
 		hello, banner
+		<div class="shadow"></div>
 	</div>
 </template>
 
@@ -17,12 +18,26 @@
 		// please romove when deploy 下方CSS请在部署前移除
 		background-color: #0080001a;
 
+		position: relative;
 		overflow: hidden;
 		width: 100%;
 		height: var(--kirakira-main-page-banner-height);
 
+		> .shadow {
+			position: absolute;
+			width: 100%;
+			height: 100%;
+			bottom: 0;
+			background: linear-gradient(#0005 80%, #000a);
+			opacity: 0;
+		}
+
 		&.collapsed {
 			height: 0;
+
+			> .shadow {
+				opacity: 1;
+			}
 		}
 	}
 </style>
