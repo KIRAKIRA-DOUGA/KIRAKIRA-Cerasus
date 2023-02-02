50897b939037a1820cbbb76eb1a6f7917099ef37
otomad <shuishenme@outlook.com>
2023-02-02 10:51:12 +0800

    Fix nitro bug

--- modules/theme/module.ts
+++ modules/theme/module.ts
@@ -17,10 +17,5 @@ export default defineNuxtModule({
 			nitro.plugins ??= [];
 			nitro.plugins.push(resolve("nitro-plugin.ts"));
 		});
-		// TODO: 截至 3.0.1 的 Nuxt 有 bug，只能暂时禁用该插件。请时刻关注该 issue 的更新：
-		// https://github.com/nuxt/framework/issues/9577
-		/* const head = nuxt.options.app.head ??= [] as typeof nuxt.options.app.head;
-		head.htmlAttrs ??= {};
-		head.htmlAttrs.class = "pink"; */
 	},
 });
