54c5b9de02f7888284f397ff701b5660dec79e9f
otomad <shuishenme@outlook.com>
2023-02-02 13:07:45 +0800

    Finish noscript mask

--- modules/noscript/module.ts
+++ modules/noscript/module.ts
@@ -1,23 +1,15 @@
-import { addTemplate, createResolver, defineNuxtModule } from "@nuxt/kit";
-import { promises } from "fs";
+import { createResolver, defineNuxtModule } from "@nuxt/kit";
+import { copyFileSync } from "fs";
 
 export default defineNuxtModule({
 	setup(_options, nuxt) {
 		const { resolve } = createResolver(import.meta.url);
-		addTemplate({
-			filename: "noscript.html",
-			write: true,
-			dst: resolve(__dirname, "../../public/noscript.html"),
-			getContents() {
-				const src = resolve(__dirname, "noscript.html");
-				return promises.readFile(src, "utf-8");
-			},
-		});
+		copyFileSync(resolve(__dirname, "noscript.html"), resolve(__dirname, "../../public/noscript.html"));
 		const noscriptPath = "/noscript.html";
 		const head = nuxt.options.app.head ??= [] as typeof nuxt.options.app.head;
 		head.noscript ??= [];
 		head.noscript.push({
-			children: `<iframe class="noscript-mask" src="${noscriptPath}"></iframe>`,
+			children: `<iframe class="noscript-mask" src="${noscriptPath}" aria-label="警告，JavaScript被禁用！"></iframe>`,
 		});
 	},
 });
