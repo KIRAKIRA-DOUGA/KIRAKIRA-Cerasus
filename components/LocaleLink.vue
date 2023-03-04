77188198b76841df796a2c3178b6ce6e351c506a
otomad <shuishenme@outlook.com>
2023-03-04 12:22:19 +0800

    Failed
    失败了，有空来修修，为什么要用 Vue 啊，React 不香吗？

--- components/LocaleLink.vue
+++ components/LocaleLink.vue
@@ -5,29 +5,11 @@
 	}>();
 
 	const localePath = useLocalePath();
-	let parent = useParent();
-	while (parent && ["RouterLink", "NuxtLink", "ObjectWrapper"].includes(parent.type.name!))
-		parent = parent.parent;
-	const scopeId = ref<string>(parent && "__scopeId" in parent.type && parent.type.__scopeId ? parent.type.__scopeId : "");
-
-	/**
-	 * 锚点 (anchor) `<a>` 标签包装器。
-	 * 为了解决 `<a>` 不能嵌套进 `<a>` 的问题。
-	 * @param _props - 组件属性。
-	 * @return JSX。
-	 */
-	const ObjectWrapper: VueJsx = (_props, { slots }) => {
-		return props.linkInLink ?
-			<object>{slots.default?.()}</object> :
-			<>{slots.default?.()}</>;
-	};
 </script>
 
 <template>
-	<ObjectWrapper>
-		<NuxtLink v-bind="{ ...$attrs, [scopeId]: '' }" :to="localePath(to)">
-			{{ parent.type.name }}
-			<slot></slot>
-		</NuxtLink>
-	</ObjectWrapper>
+	<NuxtLink v-if="!linkInLink" :to="localePath(to)"><slot></slot></NuxtLink>
+	<object v-else>
+		<NuxtLink :to="localePath(to)"><slot></slot></NuxtLink>
+	</object>
 </template>
