13aff8a99afe2c6949b3d8fbf511d01c388e3eff
Aira Sakuranomiya <1596356870@qq.com>
2023-05-11 00:21:37 +0800

    Add i18n TODO

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,7 +1,7 @@
-export default defineI18nConfig(() => ({
+export default defineI18nConfig(() => ({ // TODO: 现在i18使用的是临时版本，不然无法正常运行。https://github.com/nuxt-modules/i18n/issues/2016
 	legacy: false,
 	locale: "zh",
-	messages: {
+	messages: { // TODO: 将每个语言分别独立成单个文件。
 		zh: {
 			welcome: "欢迎",
 		},
