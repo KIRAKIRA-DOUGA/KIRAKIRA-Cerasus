b2b469a44434cb4396a3438457bc132b196847d3
Aira Sakuranomiya <1596356870@qq.com>
2023-05-13 01:19:30 +0800

    Update
    i18n修复过后的beta发布了，顺便更新一波包。
    已检查：没有因更新出现新的bug

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,4 +1,4 @@
-export default defineI18nConfig(() => ({ // TODO: 现在i18使用的是临时版本，不然无法正常运行。https://github.com/nuxt-modules/i18n/issues/2016
+export default defineI18nConfig(() => ({
 	legacy: false,
 	locale: "zh",
 	messages: { // TODO: 将每个语言分别独立成单个文件。
