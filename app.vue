38526b3a3fb1994a72e67c8143372d32b8adddb1
otomad <shuishenme@outlook.com>
2023-05-15 15:17:02 +0800

    Update more meta

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@
 	const langTag = computed(() => {
 		const langs = {
 			zh: "zh-cmn-Hans-CN", // 中文-普通话-简体字-大陆地区
-			en: "en-eng-Latn-US",
-			ja: "ja-jpn-Jpan-JP",
+			en: "en",
+			ja: "ja",
 		};
 		return langs[locale.value as keyof typeof langs] ?? locale.value;
 	});
@@ -24,7 +24,7 @@
 			{ name: "description", content: "一个可爱的视频网站，献给可爱的你～" },
 			{ name: "keywords", content: "视频,弹幕,字幕,音频,歌词,相簿,相册,照片,视频网站,弹幕视频,二次元,动漫,动画,音乐,动漫音乐,音MAD,AMV,MAD,ANIME,ACG,NOVA" },
 			// 以下内容为各种苹果私有属性。
-			{ name: "apple-mobile-web-app-title", content: "KIRAKIRA☆DOUGA" }, // 添加到主屏后的标题 (iOS)
+			{ name: "apple-mobile-web-app-title", content: "绮良动画" }, // 添加到主屏后的标题 (iOS)
 			{ name: "apple-mobile-web-app-capable", content: "yes" }, // 启用 WebApp 全屏模式 (iOS)
 			{ name: "apple-touch-fullscreen", content: "yes" }, // 启用 WebApp 全屏模式 (iOS)
 			{ name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" }, // 设置状态栏的背景颜色 (iOS)
@@ -33,12 +33,30 @@
 			// 以下内容为各种百度私有属性。
 			{ "http-equiv": "Cache-Control", content: "no-siteapp" }, // 百度禁止转码。通过百度手机打开网页时，百度可能会对你的网页进行转码，往你页面贴上它的广告，非常之恶心。
 			{ name: "referrer", content: "no-referrer" }, // 反防盗链
+			// 以下内容为开放图谱协议 (Open Graph Protocol) 属性。
+			{ property: "og:type", content: "website" },
+			{ property: "og:site_name", content: "KIRAKIRA☆DOUGA" },
+			{ property: "og:title", content: "绮良动画" },
+			{ property: "og:description", content: "一个可爱的视频网站，献给可爱的你～" },
+			{ property: "og:image", content: "https://kira.calibur.tv/static/thumbnail.png" },
+			{ property: "og:url", content: "https://kira.calibur.tv/" },
+			// 以下内容为推特私有内容属性。
+			{ name: "twitter:card", content: "summary" },
+			{ name: "twitter:site", content: "KIRAKIRA☆DOUGA" },
+			{ name: "twitter:title", content: "绮良动画" },
+			{ name: "twitter:description", content: "一个可爱的视频网站，献给可爱的你～" },
+			{ name: "twitter:image", content: "https://kira.calibur.tv/static/thumbnail.png" },
+			{ name: "twitter:url", content: "https://kira.calibur.tv/" },
 		],
 		link: [
 			{ rel: "icon", type: "image/vnd.microsoft.icon", href: "/favicon.ico" },
 			{ rel: "shortcut icon", type: "image/x-icon", href: "/favicon.ico" },
 			{ rel: "apple-touch-icon", href: "/public/static/touch/48.png" },
 			{ rel: "manifest", href: "/app.webmanifest" },
+			{ rel: "alternate", href: "https://kira.calibur.tv/", hreflang: "x-default" },
+			{ rel: "alternate", href: "https://kira.calibur.tv/", hreflang: "zh-CN" },
+			{ rel: "alternate", href: "https://kira.calibur.tv/en", hreflang: "en" },
+			{ rel: "alternate", href: "https://kira.calibur.tv/ja", hreflang: "ja" },
 		],
 	});
 
