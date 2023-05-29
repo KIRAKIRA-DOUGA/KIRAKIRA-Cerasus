6df9a911694e35dfa99505828befdba45852e59f
Aira Sakuranomiya <aira@aira.cafe>
2023-05-29 16:33:34 +0800

    Separate language from appearance

--- pages/settings/appearance.vue
+++ pages/settings/appearance.vue
@@ -1,16 +1,6 @@
 <script setup lang="ts">
-	import { LocaleObject } from "@nuxtjs/i18n/dist/runtime/composables";
 	const theme = Theme.theme;
 	const palette = Theme.palette;
-	const { locale: currentLocale, locales } = useI18n();
-	const localeModel = computed({
-		get: () => currentLocale.value,
-		set: value => switchLanguage(value),
-	});
-	const localeList = computed(() => (locales.value as LocaleObject[]).map(locale => ({
-		code: locale.code,
-		name: locale.name || locale.code,
-	})));
 	const themeList = ["light", "dark", "system"] as const;
 	const paletteList = ["pink", "sky", "blue", "orange", "purple", "green"] as const;
 </script>
@@ -35,16 +25,5 @@
 			v-ripple
 			:value="item"
 		>{{ t[item] }}</RadioButton>
-	</div>
-	<Subheader icon="translate">{{ t.language }}</Subheader>
-	<div class="chip radio-group">
-		<RadioButton
-			v-for="locale in localeList"
-			:key="locale.code"
-			v-model="localeModel"
-			v-ripple
-			:value="locale.code"
-			:lang="locale.code"
-		>{{ locale.name }}</RadioButton>
 	</div>
 </template>
---
+++ pages/settings/language.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+	import { LocaleObject } from "@nuxtjs/i18n/dist/runtime/composables";
+	const { locale: currentLocale, locales } = useI18n();
+	const localeModel = computed({
+		get: () => currentLocale.value,
+		set: value => switchLanguage(value),
+	});
+	const localeList = computed(() => (locales.value as LocaleObject[]).map(locale => ({
+		code: locale.code,
+		name: locale.name || locale.code,
+	})));
+</script>
+
+<template>
+	<div class="chip radio-group">
+		<RadioButton
+			v-for="locale in localeList"
+			:key="locale.code"
+			v-model="localeModel"
+			v-ripple
+			:value="locale.code"
+			:lang="locale.code"
+		>{{ locale.name }}</RadioButton>
+	</div>
+</template>
