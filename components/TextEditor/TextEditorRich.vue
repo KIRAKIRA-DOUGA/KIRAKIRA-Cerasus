7944985e857e9bab2c4514fc01249f8dcc55cb80
otomad <shuishenme@outlook.com>
2023-06-06 13:41:25 +0800

    Add more `scrollbar-gutter: stable`
    为啥你的 scrollbar-gutter 会写在那里？用手机修改的？

--- components/TextEditor/TextEditorRich.vue
+++ components/TextEditor/TextEditorRich.vue
@@ -23,8 +23,8 @@
 	/** 切换文本倾斜。 */
 	const toggleItalic = () => { editor.value?.chain().focus().toggleItalic().run(); };
 	/** 切换文本下划线。 */
-	const toggleUnderline = () => { editor.value?.chain().focus().toggleUnderline().run(); }; // FIXME Property 'toggleUnderline' does not exist on type 'ChainedCommands' (wtf?)
-	// FIXME: 回去重装一下完整版的 Tiptap，这个精简版本少功能。
+	const toggleUnderline = () => { editor.value?.chain().focus().toggleUnderline().run(); };
+	// FIXME: 类型“ChainedCommands”上不存在属性“toggleUnderline” (wtf?)。回去重装一下完整版的 Tiptap，这个精简版本少功能。
 	/** 切换文本删除线。 */
 	const toggleStrike = () => { editor.value?.chain().focus().toggleStrike().run(); };
 
