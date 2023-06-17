d90b5662bcde82101ee24d285a809e4b4b723ca4
cfdxkk <b2567240058@gmail.com>
2023-06-17 21:28:00 +0800

    :sparkles: ①在 API 测试页增加了 “新建用户设置档” 功能 ②追加了 api -> index.ts 用于放封装后的请求

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+export const postData = async (url: string, data: object) => {
+	const response = await fetch(url, {
+		method: "POST", // 或者 'PUT'
+		headers: {
+			"Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: JSON.stringify(data), // 将数据转换成字符串
+	});
+
+	return response.json(); // 解析为 JSON 格式
+};
--- pages/test-form.vue
+++ pages/test-form.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+	import { postData } from "~/api";
+
 	interface userSettingsType {
 		_id?: string;
 		uuid?: string;
@@ -14,11 +16,11 @@
 		results: userSettingsType;
 	}
 	useHead({ title: "API 测试页" });
-	const inputValue = ref();
+	const userIdInput = ref();
 	const userSettingsRef = ref({} as queryResultType);
 	const handleSearchUserInfo = async () => {
 		try {
-			const userId = inputValue.value;
+			const userId = userIdInput.value;
 			if (userId !== undefined && userId !== null && userId !== "") { // WARN 这里不直接用 if (userId) 是因为用户 ID 可能为数字 0
 				// 一个可用的获取用户设置信息的 URL 看起来像：https://rosales.kirakira.moe:4000/02/koa/user/settings/userSettings/get?uuid=u00001
 				const userQueryUrl = `https://rosales.kirakira.moe:4000/02/koa/user/settings/userSettings/get?uuid=${userId}`;
@@ -51,27 +53,98 @@
 			console.error("Error in handleSearchUserInfo", e);
 		}
 	};
+
+	const userSettingsInputUuid = ref("");
+	const userSettingsInputSystemStyle = ref("");
+	const userSettingsInputSystemColor = ref("");
+	const userSettingsInputBackgroundAnimation = ref(false);
+	const userSettingsInputSettingPageLastEnter = ref("");
+
+	const userSettingsSaveResult = ref();
+	const saveUserInfo = () => {
+		const userSettingsInput: userSettingsType = {
+			uuid: userSettingsInputUuid.value,
+			systemStyle: userSettingsInputSystemStyle.value,
+			systemColor: userSettingsInputSystemColor.value,
+			backgroundAnimation: userSettingsInputBackgroundAnimation.value,
+			settingPageLastEnter: userSettingsInputSettingPageLastEnter.value,
+		};
+		const uuid = userSettingsInput.uuid;
+		if (uuid) {
+			const saveUserSettingsUrl = "https://rosales.kirakira.moe:4000/02/koa/user/settings/userSettings/save";
+			postData(saveUserSettingsUrl, userSettingsInput).then(result => {
+				userSettingsSaveResult.value = result;
+			});
+		}
+	};
 </script>
 
 <template>
 	<div class="container">
+		<h2>查询用户信息</h2>
 		<div class="user-info-query-box">
 			<div style="width: 400px;">
-				<TextBox v-model="inputValue" size="normal" placeholder="输入你想查询的用户名（输入 u00001 试试看？）" />
+				<TextBox v-model="userIdInput" size="normal" placeholder="输入你想查询的用户名（输入 u00001 试试看？）" />
 			</div>
-			<Button @click="handleSearchUserInfo" icon="send">{{ t.query }}</Button>
+			<Button icon="send" @click="handleSearchUserInfo">{{ t.query }}</Button>
 		</div>
-		
 		<div style="margin-top: 10px;">{{ userSettingsRef }}</div>
+		<h2 style="margin-top: 20px;">新建用户设置档</h2>
+		<div class="user-info-save-box">
+			<div class="user-info-save-input-box">
+				<TextBox v-model="userSettingsInputUuid" size="normal" placeholder="输入用户名" />
+			</div>
+			<div class="user-info-save-input-box">
+				<TextBox v-model="userSettingsInputSystemStyle" size="normal" placeholder="系统样式" />
+			</div>
+			<div class="user-info-save-input-box">
+				<TextBox v-model="userSettingsInputSystemColor" size="normal" placeholder="背景色" />
+			</div>
+			<div class="user-info-save-input-box">
+				<ToggleSwitch v-model="userSettingsInputBackgroundAnimation">是否开启背景动画： {{ userSettingsInputBackgroundAnimation ? t.on : t.off }}</ToggleSwitch>
+			</div>
+			<div class="user-info-save-input-box">
+				<TextBox v-model="userSettingsInputSettingPageLastEnter" size="normal" placeholder="用户最后浏览的设置页" />
+			</div>
+			<Button style="margin-top: 10px;" @click="saveUserInfo">提交</Button>
+		</div>
+		<div style="margin-top: 10px;">{{ userSettingsSaveResult }}</div>
 	</div>
 </template>
 
 <style scoped lang="scss">
 	.user-info-query-box {
 		width: 500px;
+
+		margin-top: 10px;
+		
 		display: flex;
 		justify-content: space-between;
     flex-direction: row;
     align-items: center;
+	}
+
+	.user-info-save-box {
+		width: 500px;
+
+		margin-top: 10px;
+		
+		display: flex;
+		justify-content: space-between;
+    flex-direction: column;
+    align-items: flex-start;
+	}
+
+	.user-info-save-input-box {
+		width: 400px;
+		margin-top: 10px;
+	}
+
+	h1,
+	h2,
+	.page-title-wrapper {
+		color: c(accent);
+		font-weight: bold;
+		font-size: calc(1.275rem + 0.3dvw);
 	}
 </style>
