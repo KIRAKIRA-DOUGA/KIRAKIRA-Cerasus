13dcdd5e0376dff1c24e69d3977bd1bfce485b07
Aira Sakuranomiya <aira@aira.cafe>
2023-06-26 22:42:19 +0800

    Update blocklist.vue

--- pages/settings/blocklist.vue
+++ pages/settings/blocklist.vue
@@ -5,10 +5,16 @@
 <template>
 	<Subheader icon="block">屏蔽</Subheader>
 	<span>不想交流的人，他将禁止与你互动</span>
+	<section>
+		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX">被屏蔽的人</SettingsChipItem>
+	</section>
 	<Subheader icon="palette">隐藏</Subheader>
 	<span>不想看到的人，他将会从你的视角消失（除非你进入他的个人空间）</span>
+	<section>
+		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX">被屏蔽的人</SettingsChipItem>
+	</section>
 	<div class="add">
-		<TextBox v-model="add" />
+		<TextBox v-model="add" icon="person" />
 		<Button icon="add">添加</Button>
 	</div>
 </template>
