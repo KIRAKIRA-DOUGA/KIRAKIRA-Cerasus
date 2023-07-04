ad29f2971a70f9bf0ba69e8beb2a662fc205ac1a
horahoradev <horahora1567@gmail.com>
2023-07-04 15:35:51 -0700

    Video detail page working

--- pages/home.vue
+++ pages/home.vue
@@ -82,7 +82,7 @@
 			<ThumbVideo
 				v-for="video in videos?.videos"
 				:key="video.videoID"
-				:link="video.videoID?.toString() ?? ''"
+				:link="'videos' + video.videoID ?? ''"
 				:uploader="video.authorName ?? ''"
 				:image="video.thumbnailLoc"
 				:date="new Date()"
