fc513e74f9dfd45489d9c6057fa394fd3fd22b3e
horahoradev <horahora1567@gmail.com>
2023-07-07 11:34:12 -0700

    Rebase

---
+++ components/Creation/CreationComments.vue
@@ -0,0 +1,27 @@
+<docs>
+	### 稿件评论
+</docs>
+
+<script setup lang="ts">
+	const props = defineProps<{
+		/** 评论数目。 */
+		count: number | string;
+	}>();
+</script>
+
+<template>
+	<Comp>
+		<HeadingComments :count="count" />
+		<TextEditorRtf />
+	</Comp>
+</template>
+
+<style scoped lang="scss">
+	:comp {
+		margin-top: 2.5rem;
+		
+		header {
+			margin-bottom: 1rem;
+		}
+	}
+</style>
