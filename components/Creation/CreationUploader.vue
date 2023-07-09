39baad15bee7bc499cd3483c4b5d05df6f14a212
horahoradev <horahora1567@gmail.com>
2023-07-09 15:29:18 -0700

    Fix channel routing

--- components/Creation/CreationUploader.vue
+++ components/Creation/CreationUploader.vue
@@ -15,7 +15,7 @@
 
 <template>
 	<Comp>
-		<UserAvatar :avatar="avatar" :link="'/user/' + ID" />
+		<UserAvatar :avatar="avatar" :link="'/users/' + ID" />
 		<div class="text">
 			<div class="username">{{ username }}</div>
 			<div class="fans">{{ fans }} {{ t.fans }}</div>
