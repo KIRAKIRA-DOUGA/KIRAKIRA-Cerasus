2cc39932f433720524c658fcf8fc8d28d2c7ef8e
otomad <shuishenme@outlook.com>
2023-07-09 11:56:35 +0800

    Update memo style

--- utils/string.ts
+++ utils/string.ts
@@ -3,7 +3,7 @@
  * @param halfWidth - 字符串。
  * @returns - 全角字符串。
  */
-export function fullWidth(halfWidth: string) {
+export function fullwidth(halfWidth: string) {
 	return Array.from(halfWidth, char =>
 		char === " " ? "　" :
 		char.codePointAt(0)! < "!".codePointAt(0)! || char.codePointAt(0)! > "~".codePointAt(0)! ? char :
