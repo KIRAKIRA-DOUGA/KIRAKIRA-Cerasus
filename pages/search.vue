3848fa84f77d1e3a9337d9485f567320b41ca8b4
Aira Sakuranomiya <aira@aira.cafe>
2023-07-15 00:22:10 +0800

    Card padding and gap

--- pages/search.vue
+++ pages/search.vue
@@ -13,16 +13,21 @@
 		<ShadingIcon icon="search" position="right top" />
 		<HeadingGroup :name="t.search" englishName="Search" />
 		<div class="toolbox-card">
-			<ToolboxView v-model="view" />
-			<Subheader icon="sort">排序</Subheader>
-			<Sort v-model="sort">
-				<SortItem id="date" preferOrder="descending">投稿日期</SortItem>
-				<SortItem id="play" preferOrder="descending">播放数</SortItem>
-				<SortItem id="danmaku" preferOrder="descending">弹幕数</SortItem>
-				<SortItem id="comment" preferOrder="descending">评论数</SortItem>
-				<SortItem id="star" preferOrder="descending">收藏数</SortItem>
-				<SortItem id="duration">视频时长</SortItem>
-			</Sort>
+			<section>
+				<ToolboxView v-model="view" />
+			</section>
+
+			<section>
+				<Subheader icon="sort">排序</Subheader>
+				<Sort v-model="sort">
+					<SortItem id="date" preferOrder="descending">投稿日期</SortItem>
+					<SortItem id="play" preferOrder="descending">播放数</SortItem>
+					<SortItem id="danmaku" preferOrder="descending">弹幕数</SortItem>
+					<SortItem id="comment" preferOrder="descending">评论数</SortItem>
+					<SortItem id="star" preferOrder="descending">收藏数</SortItem>
+					<SortItem id="duration">视频时长</SortItem>
+				</Sort>
+			</section>
 			<Pagination v-model="page" :pages="pages" :displayPageCount="displayPageCount" enableArrowKeyMove />
 		</div>
 	</div>
