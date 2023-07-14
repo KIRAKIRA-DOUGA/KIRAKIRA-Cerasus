ec8c7c1a96d04503d182da582bd01da865c27c35
Aira Sakuranomiya <aira@aira.cafe>
2023-07-14 14:36:49 +0800

    Update blocklist

--- pages/settings/blocklist.vue
+++ pages/settings/blocklist.vue
@@ -9,8 +9,12 @@
 	<section>
 		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被封禁的人</SettingsChipItem>
 	</section>
+	<div class="add">
+		<TextBox v-model="add" icon="person" />
+		<Button icon="add">添加</Button>
+	</div>
 
-	<Subheader icon="palette">隐藏</Subheader>
+	<Subheader icon="visibility_off">隐藏</Subheader>
 	<span>不想看到的人，他将会从你的视角消失（除非你主动进入他的个人空间）</span>
 	<section>
 		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的人</SettingsChipItem>
@@ -20,33 +24,33 @@
 		<Button icon="add">添加</Button>
 	</div>
 
-	<Subheader icon="palette">标签</Subheader>
+	<Subheader icon="tag">标签</Subheader>
 	<span>不想看到的稿件标签匹配</span>
 	<section>
-		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的标签</SettingsChipItem>
+		<SettingsChipItem details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的标签</SettingsChipItem>
 	</section>
 	<div class="add">
-		<TextBox v-model="add" icon="placeholder" />
+		<TextBox v-model="add" icon="tag" />
 		<Button icon="add">添加</Button>
 	</div>
 
-	<Subheader icon="palette">关键词</Subheader>
+	<Subheader icon="key">关键词</Subheader>
 	<span>不想看到的稿件关键词匹配</span>
 	<section>
-		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的关键词</SettingsChipItem>
+		<SettingsChipItem details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的关键词</SettingsChipItem>
 	</section>
 	<div class="add">
-		<TextBox v-model="add" icon="placeholder" />
+		<TextBox v-model="add" icon="key" />
 		<Button icon="add">添加</Button>
 	</div>
 
-	<Subheader icon="palette">正则表达式</Subheader>
+	<Subheader icon="data_array">正则表达式</Subheader>
 	<span>不想看到的稿件关键词正则表达式匹配</span>
 	<section>
-		<SettingsChipItem icon="placeholder" details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的正则表达式</SettingsChipItem>
+		<SettingsChipItem details="添加日期：XXXX-XX-XX" trailingIcon="delete" @trailingIconClick="remove">被隐藏的正则表达式</SettingsChipItem>
 	</section>
 	<div class="add">
-		<TextBox v-model="add" icon="placeholder" />
+		<TextBox v-model="add" icon="data_array" />
 		<Button icon="add">添加</Button>
 	</div>
 </template>
@@ -54,6 +58,14 @@
 <style scoped lang="scss">
 	span {
 		color: c(icon-color);
+	}
+
+	.subheader {
+		margin-top: 1rem;
+
+		&:first-child {
+			margin-top: 0;
+		}
 	}
 
 	.add {
