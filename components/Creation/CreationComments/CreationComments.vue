3a9905352185a8f94e640a7a78abad603fa4d06c
Aira Sakuranomiya <aira@aira.cafe>
2023-07-21 22:00:17 +0800

    Update Comment

--- components/Creation/CreationComments/CreationComments.vue
+++ components/Creation/CreationComments/CreationComments.vue
@@ -15,12 +15,24 @@
 	});
 
 	const like = ref(0), dislike = ref(0), likeClicked = ref(false), dislikeClicked = ref(false), pinned = ref(false);
+
+	const search = ref("");
 </script>
 
 <template>
 	<Comp>
 		<HeadingComments :count="count" />
 		<TextEditorRtf :videoId="videoId" />
+		<div class="toolbar">
+			<div class="left">
+
+			</div>
+			<div class="right">
+				<SoftButton icon="deletion_history" />
+				<TextBox v-model="search" :placeholder="t.search" icon="search" />
+				<Pagination :current="1" :pages="99" :displayPageCount="7" />
+			</div>
+		</div>
 		<div class="items">
 			<CreationCommentsItem
 				v-for="comment in comments"
@@ -47,7 +59,30 @@
 		margin-top: 2.5rem;
 
 		header {
-			margin-bottom: 1rem;
+			margin-bottom: 16px;
+		}
+	}
+
+	.toolbar {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-top: 16px;
+
+		> div {
+			display: flex;
+			gap: 16px;
+			align-items: center;
+			justify-content: flex-end;
+		}
+
+		.soft-button {
+			--wrapper-size: 36px;
+			--ripple-size: 48px;
+		}
+
+		.text-box {
+			width: 200px;
 		}
 	}
 </style>
