52816074179a284378b1f69e48ef8dcd467897af
horahoradev <horahora1567@gmail.com>
2023-07-21 21:29:44 -0700

    Comment deletions

--- packages/kirakira-backend/types/ObservableAPI.ts
+++ packages/kirakira-backend/types/ObservableAPI.ts
@@ -73,6 +73,29 @@ export class ObservableDefaultApi {
     }
 
     /**
+     * Delete a comment
+     * @param id comment ID
+     */
+    public deleteComment(id: number, _options?: Configuration): Observable<void> {
+        const requestContextPromise = this.requestFactory.deleteComment(id, _options);
+
+        // build promise chain
+        let middlewarePreObservable = from<RequestContext>(requestContextPromise);
+        for (let middleware of this.configuration.middleware) {
+            middlewarePreObservable = middlewarePreObservable.pipe(mergeMap((ctx: RequestContext) => middleware.pre(ctx)));
+        }
+
+        return middlewarePreObservable.pipe(mergeMap((ctx: RequestContext) => this.configuration.httpApi.send(ctx))).
+            pipe(mergeMap((response: ResponseContext) => {
+                let middlewarePostObservable = of(response);
+                for (let middleware of this.configuration.middleware) {
+                    middlewarePostObservable = middlewarePostObservable.pipe(mergeMap((rsp: ResponseContext) => middleware.post(rsp)));
+                }
+                return middlewarePostObservable.pipe(map((rsp: ResponseContext) => this.responseProcessor.deleteComment(rsp)));
+            }));
+    }
+
+    /**
      * Log the user in
      * @param username search string
      * @param password sort category
