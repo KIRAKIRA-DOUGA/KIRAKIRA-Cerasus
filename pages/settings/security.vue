afd3cda7e2812eca42ec1ac053925e4a88fdf9ff
Aira Sakuranomiya <aira@aira.cafe>
2023-08-07 04:26:14 +0800

    Add change password modal

--- pages/settings/security.vue
+++ pages/settings/security.vue
@@ -4,6 +4,12 @@
 	const passwordChangeDateDisplay = computed(() => formatDate(passwordChangeDate.value, "yyyy-MM-dd"));
 	const authenticatorAddDateDisplay = computed(() => formatDate(authenticatorAddDate.value, "yyyy-MM-dd"));
 	const email = ref("aira@aira.cafe");
+
+	const showChangePassword = ref(false);
+
+	const oldPassword = ref("");
+	const newPassword = ref("");
+	const confirmNewPassword = ref("");
 </script>
 
 <template>
@@ -19,6 +25,7 @@
 			icon="password"
 			trailingIcon="edit"
 			:details="t.modification_date + t.colon + passwordChangeDateDisplay"
+			@trailingIconClick="showChangePassword = true"
 		>{{ t.password }}</SettingsChipItem>
 	</section>
 	<section>
@@ -28,4 +35,37 @@
 			:details="t.addition_date + t.colon + authenticatorAddDateDisplay"
 		>{{ t.authenticator }}</SettingsChipItem>
 	</section>
+
+	<Modal v-model="showChangePassword" :title="t.password_change" icon="password">
+		<div class="change-password-modal">
+			<div>
+				<TextBox v-model="oldPassword" type="password" icon="lock" :placeholder="t.password_current" />
+			</div>
+			<div>
+				<TextBox v-model="confirmNewPassword" type="password" icon="lock" :placeholder="t.password_new" />
+				<TextBox v-model="newPassword" type="password" icon="lock" :placeholder="t.password_new_retype" />
+			</div>
+		</div>
+		<template #footer-right>
+			<Button class="secondary" @click="showChangePassword = false">{{ t.step_cancel }}</Button>
+			<Button>{{ t.apply }}</Button>
+			<!-- TODO: Use a toast to show success or not, usage can be seen in page/components. -->
+			<!-- if success, the modal should be closed automaticly -->
+		</template>
+	</Modal>
 </template>
+
+<style scoped lang="scss">
+	.change-password-modal {
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+		width: 300px;
+
+		> div {
+			display: flex;
+			flex-direction: column;
+			gap: 16px;
+		}
+	}
+</style>
