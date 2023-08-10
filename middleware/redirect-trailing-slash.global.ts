6f3564ed4bd4c21802019520f15aacdb55ee9a45
otomad <shuishenme@outlook.com>
2023-08-10 11:01:02 +0800

    Update static

---
+++ middleware/redirect-trailing-slash.global.ts
@@ -0,0 +1,13 @@
+/*!
+ * 删除 URL 的尾部斜杠。
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineNuxtRouteMiddleware((to, from) => {
+	if (to.path !== "/" && to.path.endsWith("/")) {
+		const { path, query, hash } = to;
+		const nextPath = path.replace(/\/+$/, "") || "/";
+		const nextRoute = { path: nextPath, query, hash };
+		return navigateTo(nextRoute, { redirectCode: 301 });
+	}
+});
--- modules/unsupported-browsers/module.ts
+++ modules/unsupported-browsers/module.ts
@@ -1,12 +1,12 @@
-import { addImports, addPlugin, addTemplate, createResolver, defineNuxtModule } from "@nuxt/kit";
-import { readFile as _readFile } from "fs/promises";
+import { addTemplate, defineNuxtModule } from "@nuxt/kit";
 import { dirname } from "path";
-import { PREFERENTIAL_BASE_URL, PREFERENTIAL_ROUTE, TEMPLATE_PATH } from "../shared/constants";
+import { PREFERENTIAL_ROUTE, PREFERENTIAL_TEMPLATE_PATH, UNSUPPORTED_BASE_URL, UNSUPPORTED_TEMPLATE_PATH } from "../shared/constants";
 import { compileTypeScript, createReadFileResolver, minifyHtml, minifyJavaScript, useNuxtHead, wrapIife } from "../shared/encode";
 
 const IE_PAGE_HTML_FILE = "601.html";
 const IE_PAGE_SCRIPT_FILE = "jump-if-ie";
 const IE_PAGE_SCRIPT_FILE_TS = IE_PAGE_SCRIPT_FILE + ".ts", IE_PAGE_SCRIPT_FILE_JS = IE_PAGE_SCRIPT_FILE + ".js";
+const INDEX_HTML = "index.html";
 
 export default defineNuxtModule({
 	async setup(_options, nuxt) {
@@ -14,8 +14,8 @@ export default defineNuxtModule({
 
 		let iePageHtml = await readFile(IE_PAGE_HTML_FILE);
 		iePageHtml = await minifyHtml(iePageHtml);
-		addTemplate({
-			filename: TEMPLATE_PATH + IE_PAGE_HTML_FILE,
+		const template = addTemplate({
+			filename: UNSUPPORTED_TEMPLATE_PATH + INDEX_HTML,
 			write: true,
 			getContents: () => iePageHtml,
 		});
@@ -25,7 +25,7 @@ export default defineNuxtModule({
 		iePageScript = wrapIife(iePageScript);
 		iePageScript = await minifyJavaScript(iePageScript);
 		addTemplate({
-			filename: TEMPLATE_PATH + IE_PAGE_SCRIPT_FILE_JS,
+			filename: PREFERENTIAL_TEMPLATE_PATH + IE_PAGE_SCRIPT_FILE_JS,
 			write: true,
 			getContents: () => iePageScript,
 		});
@@ -35,10 +35,10 @@ export default defineNuxtModule({
 		head.script ??= [];
 		head.script.push({ src: IE_PAGE_SCRIPT_PATH });
 
-		/* nuxt.options.nitro.publicAssets = nuxt.options.nitro.publicAssets || [];
+		nuxt.options.nitro.publicAssets = nuxt.options.nitro.publicAssets || [];
 		nuxt.options.nitro.publicAssets.push({
-			baseURL: PREFERENTIAL_BASE_URL,
+			baseURL: UNSUPPORTED_BASE_URL,
 			dir: dirname(template.dst),
-		}); */
+		});
 	},
 });
