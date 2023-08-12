08645077e86e2fe2bd9a15243657a6ef1e7a7ff2
otomad <shuishenme@outlook.com>
2023-08-12 17:41:21 +0800

    Update player video

--- types/backend.d.ts
+++ types/backend.d.ts
@@ -1,8 +1,8 @@
-import { VideoDetail200Response, Comments200ResponseInner, Videos200Response } from "kirakira-backend";
+import * as _backend from "kirakira-backend";
 
 /*
  * 此处声明与后端 API 相关的全局类型。
  */
 declare global {
-	export { VideoDetail200Response, Comments200ResponseInner, Videos200Response } from "kirakira-backend";
+	export { VideoDetail200Response, Comments200ResponseInner, Videos200Response, Videos200ResponseVideosInner } from "kirakira-backend";
 }
