ee1611f54b2df24f4d87c6baf4fc1273d24e1b68
otomad <shuishenme@outlook.com>
2023-08-12 08:15:52 +0800

    Temperate test again

--- modules/unsupported-browsers/route.ts
+++ modules/unsupported-browsers/route.ts
@@ -1,19 +1,19 @@
-import { defineEventHandler, setHeader } from "h3";
+import { defineEventHandler, setHeader, sendStream } from "h3";
 import { createReadFileResolver } from "../shared/encode";
 import { IE_PAGE_HTML_FILE } from "./module";
 import { PREFERENTIAL_TEMPLATE_PATH } from "../shared/constants";
+import { createReadStream } from "fs";
 
 export default defineEventHandler(async e => {
-	const { readFile, resolve } = createReadFileResolver(import.meta.url);
-	const IE_PAGE_HTML_RELATIVE_PATH = resolve("..", PREFERENTIAL_TEMPLATE_PATH, IE_PAGE_HTML_FILE);
+	// const { readFile, resolve } = createReadFileResolver(import.meta.url);
+	// const IE_PAGE_HTML_RELATIVE_PATH = resolve("..", PREFERENTIAL_TEMPLATE_PATH, IE_PAGE_HTML_FILE);
 	
-	console.log(import.meta.url);
-	throw import.meta.url;
-
+	return sendStream(e, createReadStream("modules/unsupported-browsers/601.html"));
+	
 	setHeader(e, "Content-Type", "text/html");
 	if (!process.dev)
 		setHeader(e, "Cache-Control", "max-age=600, must-revalidate");
 
-	const iePageHtml = await readFile(IE_PAGE_HTML_RELATIVE_PATH);
-	return iePageHtml;
+	// const iePageHtml = await readFile(IE_PAGE_HTML_RELATIVE_PATH);
+	// return iePageHtml;
 });
