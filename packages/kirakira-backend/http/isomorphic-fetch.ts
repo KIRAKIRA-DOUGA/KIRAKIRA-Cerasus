1c9d9ceac969bd8167b7bae1993cee57d59c6c62
otomad <shuishenme@outlook.com>
2023-08-14 11:56:25 +0800

    Better home SSR

--- packages/kirakira-backend/http/isomorphic-fetch.ts
+++ packages/kirakira-backend/http/isomorphic-fetch.ts
@@ -1,33 +1,32 @@
-import {HttpLibrary, RequestContext, ResponseContext} from './http';
+import { HttpLibrary, RequestContext, ResponseContext } from './http';
 import { from, Observable } from '../rxjsStub';
 import { useFetch } from "nuxt/dist/app";
 import "whatwg-fetch";
 
 export class IsomorphicFetchHttpLibrary implements HttpLibrary {
 
-    public send(request: RequestContext): Observable<ResponseContext> {
-        let method = request.getHttpMethod().toString();
-        let body = request.getBody();
+	public send(request: RequestContext): Observable<ResponseContext> {
+		let method = request.getHttpMethod().toString();
+		let body = request.getBody();
 
-        const resultPromise = useFetch(request.getUrl(), {
-            method: method as any,
-            body: body as any,
-            headers: request.getHeaders(),
-            credentials: "same-origin"
-        }).then((resp: any) => {
-            const headers: { [name: string]: string } = {};
-            resp.headers.forEach((value: string, name: string) => {
-              headers[name] = value;
-            });
+		const resultPromise = useFetch(request.getUrl(), {
+			method: method as any,
+			body: body as any,
+			headers: request.getHeaders(),
+			credentials: "same-origin"
+		}).then((resp: any) => {
+			const headers: { [name: string]: string } = {};
+			resp.headers.forEach((value: string, name: string) => {
+				headers[name] = value;
+			});
 
-            const body = {
-              text: () => resp.text(),
-              binary: () => resp.blob()
-            };
-            return new ResponseContext(resp.status, headers, body);
-        });
+			const body = {
+				text: () => resp.text(),
+				binary: () => resp.blob()
+			};
+			return new ResponseContext(resp.status, headers, body);
+		});
 
-        return from<Promise<ResponseContext>>(resultPromise);
-
-    }
+		return from<Promise<ResponseContext>>(resultPromise);
+	}
 }
