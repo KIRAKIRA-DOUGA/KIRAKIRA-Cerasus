86dfc8e3cb0f94b74e09968466fb2b90d83d79f7
otomad <shuishenme@outlook.com>
2023-08-31 09:01:14 +0800

    Update module

--- modules/unsupported-browsers/jump-if-ie.ts
+++ modules/unsupported-browsers/jump-if-ie.ts
@@ -1,5 +1,9 @@
+import { UNSUPPORTED_ROUTE } from "./constants";
+
 if (window.ActiveXObject || "ActiveXObject" in window) {
-	const IE_PAGE_URL = "/unsupported?next=";
+	const IE_PAGE_URL = UNSUPPORTED_ROUTE;
+	let lang = document.documentElement.lang;
+	if (lang === "zhs") lang = "zh-cmn-Hans-CN";
 	const currentPage = encodeURIComponent(location.href);
-	location.href = IE_PAGE_URL + currentPage;
+	location.href = IE_PAGE_URL + `?lang=${lang}&next=${currentPage}`;
 }
