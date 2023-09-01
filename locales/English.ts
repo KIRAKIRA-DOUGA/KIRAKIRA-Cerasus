abce1d19d21068a679f7485028e00b98eda7ac41
Aira Sakuranomiya <aira@aira.cafe>
2023-09-01 19:46:51 +0800

    Update English.ts

--- locales/English.ts
+++ locales/English.ts
@@ -305,7 +305,7 @@ const English: LocaleIdentifiers = {
 		_: "User Page",
 		title_suffix: "'s User Page",
 	},
-	manage_content: "Manage Contents",
+	manage_content: "Manage Content",
 	add_to_blocklist: "Add to Blocklist",
 	platform: {
 		bilibili: "Bilibili",
