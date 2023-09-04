f48dca504a946c21586c8f98df3d679581a6ac4a
otomad <shuishenme@outlook.com>
2023-09-04 12:31:07 +0800

    Update offcanvas

--- components/Offcanvas.vue
+++ components/Offcanvas.vue
@@ -3,7 +3,23 @@
 </docs>
 
 <script setup lang="ts">
-	const _ = ref(); // HACK: 让 Vue 识别这个组件为组合式 API，以免产生无默认导出的 bug。
+	const shown = defineModel<boolean>({ default: false });
+
+	const drawerItems: { name: string; icon: DeclaredIcons; route?: string }[] = [
+		{ name: t.history, icon: "history" },
+		{ name: t.favorite, icon: "star" },
+		{ name: t.upload, icon: "upload", route: "/upload" },
+	];
+
+	/**
+	 * 前往路由。
+	 * @param route - 路由。
+	 */
+	function to(route?: string) {
+		if (route === undefined) return;
+		shown.value = false;
+		navigate(route);
+	}
 </script>
 
 <template>
@@ -13,7 +29,15 @@
 			<p class="username">艾了个拉</p>
 			<p class="bio">Kind and Kawaii, Forever!</p>
 		</div>
-		<div class="menu"></div>
+		<div class="grid">
+			<div v-for="item in drawerItems" :key="item.icon" v-ripple class="drawer-item" @click="to(item.route)">
+				<Icon :name="item.icon" />
+				<label>{{ item.name }}</label>
+			</div>
+		</div>
+		<div class="tab-bar vertical"><!-- 假装是 TabBar -->
+			<TabItem id="settings" v-ripple icon="settings" @click="to('/settings')">{{ t.settings }}</TabItem>
+		</div>
 	</Comp>
 </template>
 
@@ -22,16 +46,55 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
-		padding: 16px;
+		width: 50dvw;
+		padding: 24px;
 	}
 
-	.username {
-		margin: 16px 0 8px;
-		font-weight: bold;
-		font-size: 16px;
+	.user {
+		.username {
+			margin: 16px 0 8px;
+			font-weight: bold;
+			font-size: 16px;
+		}
+	
+		.bio {
+			font-size: 12px;
+		}
 	}
 	
-	.bio {
-		font-size: 12px;
+	.tab-bar {
+		margin: 0 -14px;
+	}
+	
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 8px;
+		margin: 12px 0;
+		
+		.drawer-item {
+			@include round-large;
+			@include card-in-card-shadow;
+			display: flex;
+			flex-direction: column;
+			gap: 4px;
+			align-items: center;
+			padding: 12px 0;
+			color: c(icon-color);
+			font-size: 13px;
+			background-color: c(main-bg, 50%);
+			
+			.icon {
+				font-size: 32px;
+			}
+			
+			&:any-hover {
+				@include chip-shadow;
+			}
+			
+			&:active {
+				@include button-scale-pressed;
+			}
+		}
 	}
 </style>
