836615594b1fce058973afd899dc8b3f245f3093
otomad <shuishenme@outlook.com>
2023-09-12 15:19:10 +0800

    Update i18n-editor

--- locales/editor/types/global.d.ts
+++ locales/editor/types/global.d.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { IncomingMessage, ServerResponse } from "http";
 
 declare global {
 	type ServerHandler = (request: IncomingMessage, response: ServerResponse) => boolean | void;
+	type NestedLocaleData = {
+		[key: string]: string | NestedLocaleData;
+	};
+	const bootstrap: any;
 }
 
 export { };
