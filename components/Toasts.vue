019191940d832699778a156157be623d519a0c21
Aira Sakuranomiya <aira@aira.cafe>
2023-09-14 16:39:27 +0800

    Update toast color and fix logo cover anim

--- components/Toasts.vue
+++ components/Toasts.vue
@@ -113,7 +113,7 @@
 				height: 2px;
 				border-radius: 1px;
 				animation: progressing calc(var(--duration) * 1ms) linear forwards;
-				
+
 				@media (prefers-reduced-motion: reduce) {
 					display: none;
 				}
