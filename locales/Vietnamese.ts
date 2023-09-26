b7b714e7722b7529f002d3980119bb361756108c
Cyahega (Dương Hùng) <114506758+cyahega@users.noreply.github.com>
2023-09-26 17:20:38 +0700

    Update Vietnamese translation (latest update Sep 26 2023 at 17:20PM GMT+7)

--- locales/Vietnamese.ts
+++ locales/Vietnamese.ts
@@ -270,7 +270,7 @@ const Vietnamese: LocaleIdentifiers = {
 	push_to_feed: "Dán lên tường",
 	cover: "Bìa",
 	select_cover: "Chọn bìa",
-	follow: "Đang theo dõi",
+	follow: "Theo dõi",
 	follow_verb: "Theo dõi",
 	following: "Đang theo dõi",
 	fans: "Người hâm mộ",
