fe4eed5dbb9f43219afb11eb889a0fc5b50ded2c
Aira Sakuranomiya <aira@aira.cafe>
2023-10-10 12:06:26 +0800

    Update acknowledgement.vue

--- pages/settings/acknowledgement.vue
+++ pages/settings/acknowledgement.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-	
+
 </script>
 
 <template>
 	<Subheader icon="link">{{ t.friendly_links }}</Subheader>
 	<div class="links lite-links">
 		<a href="https://otomad.wiki/"><img src="/static/images/links/otomadwiki.svg" alt="Otomad Wiki" /></a>
-		<a id="horahora" href="https://www.horahora.org/">Horahora</a>
 	</div>
 </template>
 
@@ -14,25 +13,14 @@
 	.links a {
 		@include flex-center;
 		max-width: 500px;
-		
+
 		:is(&, img) {
 			max-width: 500px;
 			transition: $fallback-transitions, scale cubic-bezier(0.18, 0.89, 0.32, 2) 500ms;
-			
+
 			&:active {
 				scale: 0.95;
 			}
-		}
-	}
-	
-	#horahora {
-		color: #0a5c4e;
-		font-weight: 700;
-		font-size: 64px;
-		font-family: $english-logo-fonts;
-		
-		html.dark & {
-			color: #9fd;
 		}
 	}
 </style>
