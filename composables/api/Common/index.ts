321501b1c6ff47537c44783164ea63c96c6a556d
cfdxkk <b2567240058@gmail.com>
2023-10-15 09:38:35 +0800

    :sparkles: 设置允许保存跨域 Cookie（临时解决方案，在生产环境不应跨域） | :musical_note: Air(初音ミク)

--- composables/api/Common/index.ts
+++ composables/api/Common/index.ts
@@ -1,4 +1,5 @@
-type HeadersObject = { [key: string]: string };
+type RequestObject = Record<string, string>;
+type HeadersObject = Record<string, string>;
 
 /**
  * 带有超时停止的 fetch
@@ -27,19 +28,21 @@ async function fetchWithTimeout(resource: RequestInfo, options: RequestInit = {}
 /**
  * 发送 GET 请求
  * @param url 请求的网址
- * @param headers 请求携带的 Header 内容
+ * @param requestOptions 请求携带的请求标头
+ * @param headerOptions 请求携带的 Header 内容
  * @param timeout 请求超时时间（毫秒），默认：30000ms
  * @returns 请取结果
  */
-export async function get(url: string, headers?: HeadersObject, timeout?: number): Promise<unknown> {
+export async function get(url: string, requestOptions: RequestObject = {}, headerOptions: HeadersObject = {}, timeout?: number): Promise<unknown> {
 	try {
 		const response = await fetchWithTimeout(url, {
 			method: "GET",
-			headers,
+			...requestOptions,
+			headers: headerOptions,
 		}, timeout);
 		return response.json();
 	} catch (error) {
-		console.error("ERROR", `something wrong in 'get', URL: ${url}`, error);
+		console.error("ERROR", `something wrong in 'get', URL: ${url}`, error); // TODO Remove Console Output?
 		throw error;
 	}
 }
@@ -48,23 +51,25 @@ export async function get(url: string, headers?: HeadersObject, timeout?: number
  * 发送 POST 请求
  * @param url 请求的网址
  * @param body 请求的 body 内容
- * @param headers 请求携带的 Header 内容
+ * @param requestOptions 请求携带的请求标头
+ * @param headerOptions 请求携带的 Header 内容
  * @param timeout 请求超时时间（毫秒），默认：30000ms
  * @returns 请求结果
  */
-export async function post(url: string, body: unknown, headers?: HeadersObject, timeout?: number): Promise<unknown> {
+export async function post(url: string, body: unknown, requestOptions: RequestObject = {}, headerOptions: HeadersObject = {}, timeout?: number): Promise<unknown> {
 	try {
 		const response = await fetchWithTimeout(url, {
 			method: "POST",
+			...requestOptions,
 			headers: {
 				"Content-Type": "application/json",
-				...headers,
+				...headerOptions,
 			},
 			body: JSON.stringify(body),
 		}, timeout);
 		return response.json();
 	} catch (error) {
-		console.error("ERROR", `something wrong in 'post', URL: ${url}`, error);
+		console.error("ERROR", `something wrong in 'post', URL: ${url}`, error); // TODO Remove Console Output?
 		throw error;
 	}
 }
--- composables/api/User/UserController.ts
+++ composables/api/User/UserController.ts
@@ -1,14 +1,16 @@
 import { get, post } from "../Common";
 import { UserExistsCheckDataDto, UserExistsCheckResultDto, UserLoginDataDto, UserLoginResultDto, UserRegistrationDataDto, UserRegistrationResultDto } from "./UserControllerDto";
 
-const USER_API_URI = "http://localhost:9999/user";
+const USER_API_URI = "https://localhost:9999/user";
 
 export const registration = async (userRegistrationData: UserRegistrationDataDto): Promise<UserRegistrationResultDto> => {
-	return await post(`${USER_API_URI}/registering`, userRegistrationData) as UserRegistrationResultDto;
+	// TODO use { credentials: "include" } to allow save cookies from cross-origin domains. Maybe we should remove it before deployment to production env.
+	return await post(`${USER_API_URI}/registering`, userRegistrationData, { credentials: "include" }) as UserRegistrationResultDto;
 };
 
 export const login = async (userLoginData: UserLoginDataDto): Promise<UserLoginResultDto> => {
-	return await post(`${USER_API_URI}/login`, userLoginData) as UserLoginResultDto;
+	// TODO use { credentials: "include" } to allow save cookies from cross-origin domains. Maybe we should remove it before deployment to production env.
+	return await post(`${USER_API_URI}/login`, userLoginData, { credentials: "include" }) as UserLoginResultDto;
 };
 
 export const userExistsCheck = async (userExistsCheckData: UserExistsCheckDataDto): Promise<UserExistsCheckResultDto> => {
