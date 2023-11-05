2f435c4399acbc37e2300d3a921e8a24a3cd9b47
otomad <shuishenme@outlook.com>
2023-11-05 22:31:29 +0800

    Optimize ImageCropper

--- components/ImageCropper.vue
+++ components/ImageCropper.vue
@@ -2,8 +2,14 @@
 	import { VueCropper } from "vue-cropper";
 	import cropTestImage from "assets/images/av820864307.jpg";
 
+	const props = withDefaults(defineProps<{
+		image?: string;
+	}>(), {
+		image: cropTestImage,
+	});
+
 	const vueCropperOption = reactive({
-		img: cropTestImage,
+		img: props.image,
 		size: 1,
 		full: false,
 		outputType: "png",
@@ -50,124 +56,154 @@
 </template>
 
 <style scoped lang="scss">
-	.vue-cropper {
-		&:deep(*) {
-			// 防止拖动出现延迟
-			transition: none;
+	.vue-cropper:deep(*) {
+		// 防止拖动出现延迟
+		transition: none;
 
-			.cropper-view-box {
-				outline-color: c(accent);
+		.cropper-view-box {
+			outline-color: c(accent);
 
-				// 网格线
-				&::before,
-				&::after {
-					position: absolute;
-					z-index: 1;
-					content: "";
-					pointer-events: none;
-				}
-
-				// 网格线 - 横向
-				&::before {
-					top: 50%;
-					left: 0;
-					width: 100%;
-					height: 33%;
-					border-top: 1px dashed c(accent);
-					border-bottom: 1px dashed c(accent);
-					transform: translateY(-50%);
-				}
-
-				// 网格线 - 纵向
-				&::after {
-					top: 0;
-					left: 50%;
-					width: 33%;
-					height: 100%;
-					border-right: 1px dashed c(accent);
-					border-left: 1px dashed c(accent);
-					transform: translateX(-50%);
-				}
+			// 网格线
+			&::before,
+			&::after {
+				position: absolute;
+				z-index: 1;
+				border: dotted c(accent);
+				content: "";
+				pointer-events: none;
 			}
 
-			.cropper-face {
-				@include flex-center;
-				background-color: transparent;
-				opacity: 1;
-
-				// 准星
-				&::before,
-				&::after {
-					@include oval;
-					position: absolute;
-					display: block;
-					background-color: c(accent);
-					content: "";
-					pointer-events: none;
-				}
-
-				// 准星 - 横向
-				&::before {
-					width: 10px;
-					height: 1px;
-				}
-
-				// 准星 - 纵向
-				&::after {
-					width: 1px;
-					height: 10px;
-				}
+			// 网格线 - 横向
+			&::before {
+				top: 50%;
+				left: 0;
+				width: 100%;
+				height: calc(100% / 3);
+				border-width: 1px 0;
+				translate: 0 -50%;
 			}
 
-			// 控制球
-			.crop-point {
-				@include circle;
-				@include flex-center;
-				@include control-ball-shadow;
-				background-color: c(main-bg);
-				opacity: 1;
-				transition: $fallback-transitions;
-
-				&::before {
-					@include circle;
-					display: block;
-					width: 100%;
-					height: 100%;
-					background-color: c(accent);
-					content: "";
-					scale: 0.5;
-				}
-
-				&:hover::before {
-					scale: 0.7;
-				}
-
-				&:active::before {
-					scale: 0.4;
-				}
-
-				&:focus,
-				&:active {
-					@include large-shadow-focus;
-				}
-			}
-
-			// 分辨率信息
-			.crop-info {
-				@include round-small;
-				@include dropdown-flyouts;
-				top: unset !important;
-				bottom: -8px;
+			// 网格线 - 纵向
+			&::after {
+				top: 0;
 				left: 50%;
-				padding: 8px 10px;
-				color: c(text);
-				font-size: 14px;
-				white-space: nowrap;
-				background-color: c(acrylic-bg, 75%);
-				transform: translateX(-50%);
-				translate: 0 36px;
-				font-variant-numeric: tabular-nums;
+				width: calc(100% / 3);
+				height: 100%;
+				border-width: 0 1px;
+				translate: -50%;
 			}
+		}
+
+		.cropper-face {
+			@include flex-center;
+			background-color: transparent;
+			opacity: 1;
+
+			// 准星
+			&::before,
+			&::after {
+				@include oval;
+				position: absolute;
+				display: block;
+				background-color: c(accent);
+				content: "";
+				pointer-events: none;
+			}
+
+			// 准星 - 横向
+			&::before {
+				width: 12px;
+				height: 1px;
+			}
+
+			// 准星 - 纵向
+			&::after {
+				width: 1px;
+				height: 12px;
+			}
+		}
+
+		// 控制球
+		.crop-point {
+			$size: 16px;
+			// stylelint-disable-next-line order/order
+			@include square($size);
+			@include circle;
+			@include flex-center;
+			@include control-ball-shadow;
+			background-color: c(main-bg);
+			opacity: 1;
+			transition: $fallback-transitions;
+
+			&::before {
+				@include circle;
+				display: block;
+				width: 100%;
+				height: 100%;
+				background-color: c(accent);
+				content: "";
+				scale: 0.5;
+			}
+
+			&:hover::before {
+				scale: 0.7;
+			}
+
+			&:active::before {
+				scale: 0.4;
+			}
+
+			&:focus,
+			&:active {
+				@include large-shadow-focus;
+			}
+
+			@for $i from 1 through 8 {
+				$offset: calc($size / -2);
+				$offset-center: calc(50% - $size / 4);
+
+				&.point#{$i} {
+					@if $i <= 3 {
+						top: $offset;
+					}
+
+					@if $i >= 6 {
+						bottom: $offset;
+					}
+
+					@if list.index(1 4 6, $i) {
+						left: $offset;
+					}
+
+					@if list.index(3 5 8, $i) {
+						right: $offset;
+					}
+
+					@if list.index(2 7, $i) {
+						left: $offset-center;
+					}
+
+					@if list.index(4 5, $i) {
+						top: $offset-center;
+					}
+				}
+			}
+		}
+
+		// 分辨率信息
+		.crop-info {
+			@include round-small;
+			@include dropdown-flyouts;
+			top: unset !important;
+			bottom: -8px;
+			left: 50%;
+			padding: 8px 10px;
+			color: c(text-color);
+			font-size: 14px;
+			white-space: nowrap;
+			background-color: c(acrylic-bg, 75%);
+			translate: -50% 44px;
+			font-variant-numeric: tabular-nums;
 		}
 	}
 </style>
