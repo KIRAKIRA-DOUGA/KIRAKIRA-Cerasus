0d16db39449abe76e2143761d58b8bce4cfa1e45
otomad <shuishenme@outlook.com>
2023-11-05 23:20:24 +0800

    Remove unnecessary type declarations

--- stores/user-info.ts
+++ stores/user-info.ts
@@ -2,7 +2,7 @@ export const useUserInfoStore = defineStore("user-info", () => {
 	/** 是否已经登录？ */
 	const isLogined = ref(false);
 	/** 用户名。 */
-	const username = ref<string>("");
+	const username = ref("");
 
 	return { isLogined, username };
 });
