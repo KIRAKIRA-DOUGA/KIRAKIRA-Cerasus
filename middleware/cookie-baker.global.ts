daadf9929aee170e09c236a31eed4542b1ee5b19
cfdxkk <b2567240058@gmail.com>
2024-02-12 14:01:55 +0800

    :recycle: :sparkles: :construction: [重大更新] [重构] [实验性] 持久化的，用户与用户自定主题

--- middleware/cookie-baker.global.ts
+++ middleware/cookie-baker.global.ts
@@ -1,4 +1,4 @@
-import { COOKIE_NOT_FOUND, CUSTOMER_THEME_COLOR, DEFAULT_THEME_COLOR, SYSTEM_THEME, coloredSidebarCookieKey, customerThemeColorCookieKey, themeColorCookieKey, themeTypeCookieKey } from "~/modules/theme/cookieBinding";
+import { CUSTOMER_THEME_COLOR, DEFAULT_THEME_COLOR, SYSTEM_THEME, coloredSidebarCookieKey, customerThemeColorCookieKey, isOfflineSettingsCookieKey, themeColorCookieKey, themeTypeCookieKey } from "~/modules/theme/cookieBinding";
 import { PALETTE_LIST } from "~/modules/theme/types";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
@@ -12,11 +12,15 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 			const cookieUid = useCookie(uidCookieKey, { sameSite: true });
 			const cookieToken = useCookie(tokenCookieKey, { sameSite: true });
 
+			const userSettingsCookieBasicOption = { expires: new Date("9999/9/9"), sameSite: true, httpOnly: false };
 			// Nuxt cookie 对象 - 用户样式设置
-			const cookieThemeType = useCookie(themeTypeCookieKey, { expires: new Date("9999/9/9"), sameSite: true, httpOnly: false });
-			const cookieThemeColor = useCookie(themeColorCookieKey, { expires: new Date("9999/9/9"), sameSite: true, httpOnly: false });
-			const cookieCustomerThemeColor = useCookie(customerThemeColorCookieKey, { expires: new Date("9999/9/9"), sameSite: true, httpOnly: false });
-			const cookieColoredSidebar = useCookie(coloredSidebarCookieKey, { expires: new Date("9999/9/9"), sameSite: true, httpOnly: false });
+			const cookieThemeType = useCookie(themeTypeCookieKey, userSettingsCookieBasicOption);
+			const cookieThemeColor = useCookie(themeColorCookieKey, userSettingsCookieBasicOption);
+			const cookieCustomerThemeColor = useCookie(customerThemeColorCookieKey, userSettingsCookieBasicOption);
+			const cookieColoredSidebar = useCookie(coloredSidebarCookieKey, userSettingsCookieBasicOption);
+
+			// nuxt cookie 对象 - 是否使用离线样式设置
+			const cookieIsLocalStorage = useCookie(isOfflineSettingsCookieKey, userSettingsCookieBasicOption);
 			
 			const uid = cookieUid.value;
 			const token = cookieToken.value;
@@ -34,13 +38,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 				cookieThemeColor.value = userSettings?.userSettings?.themeColor ? (PALETTE_LIST as unknown as string[]).includes(userSettings.userSettings.themeColor) ? userSettings.userSettings.themeColor : CUSTOMER_THEME_COLOR : DEFAULT_THEME_COLOR;
 				cookieCustomerThemeColor.value = userSettings?.userSettings?.themeColor || "";
 				cookieColoredSidebar.value = `${userSettings?.userSettings?.coloredSideBar}`;
-			} else {
-				// 如果用户认证 cookie 不存在，则尝试续期（重新赋值）离线用户样式设置 cookie，如果连离线用户样式设置 cookie 都不存在了，则存储一个特殊标识字符串：“404”，在后续的客户端渲染处理步骤中，对于 “404” 的 cookie，会尝试从 localStorage 里获取
-				cookieThemeType.value = cookieThemeType.value === undefined ? COOKIE_NOT_FOUND : cookieThemeType.value;
-				cookieThemeColor.value = cookieThemeColor.value === undefined ? COOKIE_NOT_FOUND : cookieThemeColor.value;
-				cookieCustomerThemeColor.value = cookieCustomerThemeColor.value === undefined ? COOKIE_NOT_FOUND : cookieCustomerThemeColor.value;
-				cookieColoredSidebar.value = cookieColoredSidebar.value === undefined ? COOKIE_NOT_FOUND : cookieColoredSidebar.value;
-			}
+
+				cookieIsLocalStorage.value = "false";
+			} else
+				cookieIsLocalStorage.value = "true";
 		}
 	} catch (error) {
 		console.error("ERROR", "烘焙网络曲奇☆时发生了意料外的错误：", error);
--- modules/theme/cookieBinding.ts
+++ modules/theme/cookieBinding.ts
@@ -1,5 +1,4 @@
 // theme 关键字
-export const COOKIE_NOT_FOUND = "404";
 export const SYSTEM_THEME = "system";
 export const DEFAULT_THEME_COLOR = "pink";
 export const CUSTOMER_THEME_COLOR = "customer";
@@ -12,6 +11,7 @@ export const themeTypeCookieKey = "theme-type";
 export const themeColorCookieKey = "theme-color";
 export const customerThemeColorCookieKey = "customer-theme-color";
 export const coloredSidebarCookieKey = "colored-side-bar";
+export const isOfflineSettingsCookieKey = "is-offline-settings";
 
 /**
  * 在 DOM 加载之前执行的脚本。
@@ -19,7 +19,6 @@ export const coloredSidebarCookieKey = "colored-side-bar";
  */
 export function cookieBinding() {
 	// theme 关键字，请和上方全局变量中的一致
-	const COOKIE_NOT_FOUND = "404";
 	const SYSTEM_THEME = "system";
 	const DEFAULT_THEME_COLOR = "pink";
 	const CUSTOMER_THEME_COLOR = "customer";
@@ -32,6 +31,7 @@ export function cookieBinding() {
 	const themeColorCookieKey = "theme-color";
 	const customerThemeColorCookieKey = "customer-theme-color";
 	const coloredSidebarCookieKey = "colored-side-bar";
+	const isOfflineSettingsCookieKey = "is-offline-settings";
 
 	/**
 	 * 用原始的方式获取 cookie
@@ -48,46 +48,54 @@ export function cookieBinding() {
 		return ""; // 如果没有找到指定的 cookie，返回 "" 空字符串
 	};
 
-	/**
-	 * 深层数据挖掘机 - 如果你要获取的 cookie 的值为 COOKIE_NOT_FOUND 时，则尝试去 localStorage 中获取
-	 * @param cookieName key
-	 * @returns 根据 cookieName 去 cookie 或者 localStorage 中获取的数据
-	 */
-	const deepDataMiner = (cookieName: string): string => {
-		const cookieValue = getCookie(cookieName);
-		if (cookieValue === COOKIE_NOT_FOUND)
-			return window.localStorage.getItem(cookieName) || "";
-		else
-			return cookieValue;
-	};
-
 	try {
-		// 调用深层数据挖掘机，尽可能地获取 cookie 或 localStorage 中的用户样式设置
+		const isOfflineSettings = !getCookie(isOfflineSettingsCookieKey) || getCookie(isOfflineSettingsCookieKey) === "true";
+
 		const systemThemeType = window.matchMedia("(prefers-color-scheme: dark)").matches ? THEME_DARK : THEME_LIGHT; // 获取系统主题类型（当 themeType 的值为 system 时才应该依据该值渲染）
-		const currentThemeType = (deepDataMiner(themeTypeCookieKey) || systemThemeType) as ThemeSetType; // 客户端中存储的系统主题类型，如果没有，则使用 systemThemeType
 
-		const themeColor = (deepDataMiner(themeColorCookieKey) || DEFAULT_THEME_COLOR) as PaletteType; // 客户端中存储的系统主题色
-		const customerThemeColor = deepDataMiner(customerThemeColorCookieKey) as string; // 客户端中存储的自定义系统主题色（当 themeColor 的值为 CUSTOMER_THEME_COLOR 时才应该依据该值渲染）
+		let currentThemeType;
+		let themeColor;
+		let customerThemeColor;
+		let isColoredSidebar;
 
-		const isColoredSidebar = deepDataMiner(coloredSidebarCookieKey) === "true"; // 客户端中存储的是否启用彩色侧边栏
+		if (isOfflineSettings) { // 离线样式，从 localStorage 中获取样式并拷贝到 cookie 中
+			// 获取 localStorage 中的用户样式设置
 
-		// 开始绑定样式
+			currentThemeType = window.localStorage.getItem(themeTypeCookieKey); // localStorage 中存储的系统主题类型，如果没有，则使用 systemThemeType
+			themeColor = window.localStorage.getItem(themeColorCookieKey); // localStorage 中存储的系统主题色
+			customerThemeColor = window.localStorage.getItem(customerThemeColorCookieKey); // localStorage 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOMER_THEME_COLOR 时才应该依据该值渲染）
+			isColoredSidebar = window.localStorage.getItem(coloredSidebarCookieKey); // localStorage 中存储的是否启用彩色侧边栏
+
+			// 将最新的 localStorage 存储回 cookie
+			const userSettingsCookieBasicOption = document.cookie = `; expires=${new Date("9999/9/9").toUTCString()}; path=/; SameSite=Strict`;
+			if (currentThemeType) document.cookie = `${themeTypeCookieKey}=${currentThemeType}${userSettingsCookieBasicOption}`;
+			if (themeColor) document.cookie = `${themeColorCookieKey}=${themeColor}${userSettingsCookieBasicOption}`;
+			if (customerThemeColor) document.cookie = `${customerThemeColorCookieKey}=${customerThemeColor}${userSettingsCookieBasicOption}`;
+			if (isColoredSidebar) document.cookie = `${coloredSidebarCookieKey}=${isColoredSidebar}${userSettingsCookieBasicOption}`;
+		} else {
+			// 获取 cookie 中的用户样式设置
+			currentThemeType = (getCookie(themeTypeCookieKey) || systemThemeType) as ThemeSetType; // 客户端中存储的系统主题类型，如果没有，则使用 systemThemeType
+			themeColor = (getCookie(themeColorCookieKey) || DEFAULT_THEME_COLOR) as PaletteType; // 客户端中存储的系统主题色
+			customerThemeColor = getCookie(customerThemeColorCookieKey) as string; // 客户端中存储的自定义系统主题色（当 themeColor 的值为 CUSTOMER_THEME_COLOR 时才应该依据该值渲染）
+			isColoredSidebar = getCookie(coloredSidebarCookieKey) === "true"; // 客户端中存储的是否启用彩色侧边栏
+
+			// 将最新的 cookie 存储回 localStorage
+			if (currentThemeType) window.localStorage.setItem(themeTypeCookieKey, currentThemeType);
+			if (themeColor) window.localStorage.setItem(themeColorCookieKey, themeColor);
+			if (customerThemeColor) window.localStorage.setItem(customerThemeColorCookieKey, customerThemeColor);
+			if (isColoredSidebar) window.localStorage.setItem(coloredSidebarCookieKey, `${isColoredSidebar}`);
+		}
+
+		// 绑定样式
 		const rootNode = document.documentElement;
 		rootNode.className = "kirakira";
 		if (currentThemeType) rootNode.classList.add(currentThemeType);
-		if (themeColor === CUSTOMER_THEME_COLOR && customerThemeColor)
+		if (themeColor && themeColor === CUSTOMER_THEME_COLOR && customerThemeColor)
 			// TODO 设置自定义主题色
 			console.log("themeCustomerColor", customerThemeColor);
-		else
+		else if (themeColor)
 			rootNode.classList.add(themeColor);
 		if (isColoredSidebar) rootNode.classList.add("colored-sidebar");
-
-		// TODO // FIXME 在用户未登录或未开启多端同步的状态下存在问题：虽然 cookie 被设为 404 后，正确地从 localStorage 中获取值并渲染了，但 cookie 此时仍为 404，而 localStorage 中则被设为 404，在下次渲染时会把 404 发送到服务端，然后出错
-		// 将最新的 cookie 存储回 localStorage
-		if (currentThemeType) window.localStorage.setItem(themeTypeCookieKey, currentThemeType);
-		if (themeColor) window.localStorage.setItem(themeColorCookieKey, themeColor);
-		if (customerThemeColor) window.localStorage.setItem(customerThemeColorCookieKey, customerThemeColor);
-		if (isColoredSidebar) window.localStorage.setItem(coloredSidebarCookieKey, `${isColoredSidebar}`);
 	} catch (error) {
 		console.error("ERROR", "ERROR IN cookieBander", error);
 		try {
