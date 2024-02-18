c93980a33a95d4271c9d5b48fd9a8c71714b64fc
cfdxkk <b2567240058@gmail.com>
2024-02-18 21:47:05 +0800

    :recycle: :sparkles: :bug: 修复：①无法实时跟随系统主题的 bug ② 未登录用户设定主题跟随系统但该设置没有持久化的 bug

--- modules/theme/cookieBinding.ts
+++ modules/theme/cookieBinding.ts
@@ -67,8 +67,6 @@ export function cookieBinding() {
 	};
 
 	try {
-		const systemThemeType = window.matchMedia("(prefers-color-scheme: dark)").matches ? THEME_DARK : THEME_LIGHT; // 获取系统主题类型（当 themeType 的值为 system 时才应该依据该值渲染）
-
 		let currentThemeType;
 		let themeColor;
 		let customerThemeColor;
@@ -80,7 +78,7 @@ export function cookieBinding() {
 		const isOfflineSettings = !getCookie(isOfflineSettingsCookieKey) || getCookie(isOfflineSettingsCookieKey) === "true";
 		if (isOfflineSettings) { // 离线样式，从 localStorage 中获取样式并拷贝到 cookie 中
 			// 获取 localStorage 中的用户样式设置
-			currentThemeType = (window.localStorage.getItem(themeTypeCookieKey) && window.localStorage.getItem(themeTypeCookieKey) === "system") ? systemThemeType : (window.localStorage.getItem(themeTypeCookieKey) || THEME_LIGHT); // localStorage 中存储的系统主题类型，如果没有或值为 system，则使用 systemThemeType
+			currentThemeType = window.localStorage.getItem(themeTypeCookieKey) || THEME_LIGHT; // localStorage 中存储的系统主题类型，如果没有或值为 system，则使用 systemThemeType
 			themeColor = window.localStorage.getItem(themeColorCookieKey) || DEFAULT_THEME_COLOR; // localStorage 中存储的系统主题色
 			customerThemeColor = window.localStorage.getItem(customThemeColorCookieKey) || DEFAULT_THEME_COLOR; // localStorage 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOM_THEME_COLOR 时才应该依据该值渲染）
 			isColoredSidebar = window.localStorage.getItem(coloredSidebarCookieKey) || NO_COLORED_SIDEBAR; // localStorage 中存储的是否启用彩色侧边栏
@@ -99,7 +97,7 @@ export function cookieBinding() {
 			// HACK 11 在此处添加
 		} else { // 在线（远程同步）样式，从 cookie 中获取样式并拷贝到 localStorage 中
 			// 获取 cookie 中的用户样式设置
-			currentThemeType = ((getCookie(themeTypeCookieKey) && getCookie(themeTypeCookieKey) === "system") ? systemThemeType : (getCookie(themeTypeCookieKey)) || THEME_LIGHT) as ThemeSetType; // cookie 中存储的系统主题类型，如果没有，则使用 systemThemeType
+			currentThemeType = (getCookie(themeTypeCookieKey) || THEME_LIGHT) as ThemeSetType; // cookie 中存储的系统主题类型，如果没有，则使用 systemThemeType
 			themeColor = (getCookie(themeColorCookieKey) || DEFAULT_THEME_COLOR) as PaletteType; // cookie 中存储的系统主题色
 			customerThemeColor = (getCookie(customThemeColorCookieKey) || DEFAULT_THEME_COLOR) as string; // cookie 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOM_THEME_COLOR 时才应该依据该值渲染）
 			isColoredSidebar = (getCookie(coloredSidebarCookieKey) || NO_COLORED_SIDEBAR); // cookie 中存储的是否启用彩色侧边栏
@@ -117,10 +115,12 @@ export function cookieBinding() {
 			// HACK 13 在此处添加
 		}
 
+		const systemThemeType = window.matchMedia("(prefers-color-scheme: dark)").matches ? THEME_DARK : THEME_LIGHT; // 获取系统主题类型（当 themeType 的值为 system 时才应该依据该值渲染）
+
 		// 绑定 cookie 中的样式到 html
 		const rootNode = document.documentElement;
 		rootNode.className = "kirakira";
-		if (currentThemeType) rootNode.classList.add(currentThemeType);
+		if (currentThemeType && currentThemeType !== "system") rootNode.classList.add(currentThemeType); else if (currentThemeType && currentThemeType === "system") rootNode.classList.add(systemThemeType);
 		if (themeColor && themeColor === CUSTOM_THEME_COLOR && customerThemeColor)
 			console.log("themeCustomerColor", customerThemeColor); // TODO 设置自定义主题色
 		else if (themeColor)
--- modules/theme/plugin.ts
+++ modules/theme/plugin.ts
@@ -1,5 +1,8 @@
+import { cookieBinding } from "./cookieBinding";
+
 export default defineNuxtPlugin(nuxt => {
 	nuxt.hook("app:mounted", () => {
-		// do something
+		// 监听系统主题的 change 事件，如果系统主题变更，也要重新绑定样式
+		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", cookieBinding);
 	});
 });
