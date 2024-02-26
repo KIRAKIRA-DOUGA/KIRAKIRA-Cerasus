29408ccb80e479ffc72071f78d4c37c7c1dc10e6
cfdxkk <b2567240058@gmail.com>
2024-02-26 20:00:18 +0800

    :ambulance: :bug: Hotfix: 修复一个切换某些设置项会导致主题在明暗之间来回切换的 bug :cry: :pencil2: 降低代码异味

--- modules/theme/theme-cookie-binding.ts
+++ modules/theme/theme-cookie-binding.ts
@@ -7,12 +7,12 @@ export const DEFAULT_COOKIE_OPTION = { expires: new Date("9999/9/9"), sameSite: 
 export const THEME_ENV = {
 	SYSTEM_THEME: "system",
 	DEFAULT_THEME_COLOR: "pink",
-	CUSTOM_THEME_COLOR: "customer",
+	CUSTOM_THEME_COLOR: "custom",
 	// DEFAULT_CUSTOM_THEME_COLOR: "66CCFF", // TODO 设置默认自定义主题色
 	THEME_DARK: "dark",
 	THEME_LIGHT: "light",
 	NO_COLORED_SIDEBAR: "false",
-	NO_SHARP_APPEARANCE_MODE: "false", // FIXME: 不是已经没有实验性数据了吗！
+	NO_SHARP_APPEARANCE_MODE: "false",
 	NO_FLAT_APPEARANCE_MODE: "false",
 	// HACK 1 在此处添加
 };
@@ -42,7 +42,7 @@ export function cookieBinding() {
 	// theme 默认值，请保持和上方 THEME_ENV 全局变量的值一致
 	const SYSTEM_THEME = "system";
 	const DEFAULT_THEME_COLOR = "pink";
-	const CUSTOM_THEME_COLOR = "customer"; // FIXME: 请改为 "custom"！
+	const CUSTOM_THEME_COLOR = "custom";
 	// const DEFAULT_CUSTOM_THEME_COLOR = "66CCFF"; // TODO 设置默认自定义主题色
 	const THEME_DARK = "dark";
 	const THEME_LIGHT = "light";
@@ -77,12 +77,12 @@ export function cookieBinding() {
 	};
 
 	try {
-		let currentThemeType;
-		let themeColor;
-		let customerThemeColor; // FIXME: "customThemeColor"
-		let isColoredSidebar;
-		let isSharpAppearanceMode;
-		let isFlatAppearanceMode;
+		let currentThemeType; // 主题（浅色/深色）
+		let themeColor; // 个性色
+		let customThemeColor; // 用户自定义个性色
+		let isColoredSidebar; // 是否启用彩色侧边栏
+		let isSharpAppearanceMode; // 是否启用直角模式
+		let isFlatAppearanceMode; // 是否启用扁平模式
 		// HACK 9 在此处添加
 
 		const isOfflineSettings = !getCookie(isOfflineSettingsCookieKey) || getCookie(isOfflineSettingsCookieKey) === "true";
@@ -90,7 +90,7 @@ export function cookieBinding() {
 			// 获取 localStorage 中的用户样式设置
 			currentThemeType = window.localStorage.getItem(themeTypeCookieKey) || THEME_LIGHT; // localStorage 中存储的系统主题类型，如果没有或值为 system，则使用 systemThemeType
 			themeColor = window.localStorage.getItem(themeColorCookieKey) || DEFAULT_THEME_COLOR; // localStorage 中存储的系统主题色
-			customerThemeColor = window.localStorage.getItem(customThemeColorCookieKey) || DEFAULT_THEME_COLOR; // localStorage 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOM_THEME_COLOR 时才应该依据该值渲染）
+			customThemeColor = window.localStorage.getItem(customThemeColorCookieKey) || DEFAULT_THEME_COLOR; // localStorage 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOM_THEME_COLOR 时才应该依据该值渲染）
 			isColoredSidebar = window.localStorage.getItem(coloredSidebarCookieKey) || NO_COLORED_SIDEBAR; // localStorage 中存储的是否启用彩色侧边栏
 			isSharpAppearanceMode = window.localStorage.getItem(sharpAppearanceModeCookieKey) || NO_SHARP_APPEARANCE_MODE; // localStorage 中存储的是否启用直角模式
 			isFlatAppearanceMode = window.localStorage.getItem(flatAppearanceModeCookieKey) || NO_FLAT_APPEARANCE_MODE; // localStorage 中存储的是否启用扁平模式
@@ -100,7 +100,7 @@ export function cookieBinding() {
 			const userSettingsCookieBasicOption = `; expires=${new Date("9999/9/9").toUTCString()}; path=/; SameSite=Strict`;
 			if (currentThemeType) document.cookie = `${themeTypeCookieKey}=${currentThemeType}${userSettingsCookieBasicOption}`;
 			if (themeColor) document.cookie = `${themeColorCookieKey}=${themeColor}${userSettingsCookieBasicOption}`;
-			if (customerThemeColor) document.cookie = `${customThemeColorCookieKey}=${customerThemeColor}${userSettingsCookieBasicOption}`;
+			if (customThemeColor) document.cookie = `${customThemeColorCookieKey}=${customThemeColor}${userSettingsCookieBasicOption}`;
 			if (isColoredSidebar !== undefined && isColoredSidebar !== null) document.cookie = `${coloredSidebarCookieKey}=${isColoredSidebar}${userSettingsCookieBasicOption}`;
 			if (isSharpAppearanceMode !== undefined && isSharpAppearanceMode !== null) document.cookie = `${sharpAppearanceModeCookieKey}=${isSharpAppearanceMode}${userSettingsCookieBasicOption}`;
 			if (isFlatAppearanceMode !== undefined && isFlatAppearanceMode !== null) document.cookie = `${flatAppearanceModeCookieKey}=${isFlatAppearanceMode}${userSettingsCookieBasicOption}`;
@@ -109,7 +109,7 @@ export function cookieBinding() {
 			// 获取 cookie 中的用户样式设置
 			currentThemeType = (getCookie(themeTypeCookieKey) || THEME_LIGHT) as ThemeSetType; // cookie 中存储的系统主题类型，如果没有，则使用 systemThemeType
 			themeColor = (getCookie(themeColorCookieKey) || DEFAULT_THEME_COLOR) as PaletteType; // cookie 中存储的系统主题色
-			customerThemeColor = (getCookie(customThemeColorCookieKey) || DEFAULT_THEME_COLOR) as string; // cookie 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOM_THEME_COLOR 时才应该依据该值渲染）
+			customThemeColor = (getCookie(customThemeColorCookieKey) || DEFAULT_THEME_COLOR) as string; // cookie 中存储的自定义系统主题色（当 themeColor 的值为 CUSTOM_THEME_COLOR 时才应该依据该值渲染）
 			isColoredSidebar = (getCookie(coloredSidebarCookieKey) || NO_COLORED_SIDEBAR); // cookie 中存储的是否启用彩色侧边栏
 			isSharpAppearanceMode = (getCookie(sharpAppearanceModeCookieKey) || NO_SHARP_APPEARANCE_MODE); // cookie 中存储的是否启用直角模式
 			isFlatAppearanceMode = (getCookie(flatAppearanceModeCookieKey) || NO_FLAT_APPEARANCE_MODE); // cookie 中存储的是否启用扁平模式
@@ -118,7 +118,7 @@ export function cookieBinding() {
 			// 将最新的 cookie 存储回 localStorage（以备以后用户登出后使用）
 			if (currentThemeType) window.localStorage.setItem(themeTypeCookieKey, currentThemeType); else window.localStorage.setItem(themeTypeCookieKey, THEME_LIGHT);
 			if (themeColor) window.localStorage.setItem(themeColorCookieKey, themeColor); else window.localStorage.setItem(themeColorCookieKey, DEFAULT_THEME_COLOR);
-			if (customerThemeColor) window.localStorage.setItem(customThemeColorCookieKey, customerThemeColor); else window.localStorage.setItem(customThemeColorCookieKey, DEFAULT_THEME_COLOR);
+			if (customThemeColor) window.localStorage.setItem(customThemeColorCookieKey, customThemeColor); else window.localStorage.setItem(customThemeColorCookieKey, DEFAULT_THEME_COLOR);
 			if (isColoredSidebar !== undefined && isColoredSidebar !== null) window.localStorage.setItem(coloredSidebarCookieKey, `${isColoredSidebar}`); else window.localStorage.setItem(coloredSidebarCookieKey, NO_COLORED_SIDEBAR);
 			if (isSharpAppearanceMode !== undefined && isSharpAppearanceMode !== null) window.localStorage.setItem(sharpAppearanceModeCookieKey, `${isSharpAppearanceMode}`); else window.localStorage.setItem(sharpAppearanceModeCookieKey, NO_SHARP_APPEARANCE_MODE);
 			if (isFlatAppearanceMode !== undefined && isFlatAppearanceMode !== null) window.localStorage.setItem(flatAppearanceModeCookieKey, `${isFlatAppearanceMode}`); else window.localStorage.setItem(flatAppearanceModeCookieKey, NO_FLAT_APPEARANCE_MODE);
@@ -130,12 +130,11 @@ export function cookieBinding() {
 		// 绑定 cookie 中的样式到 html
 		const rootNode = document.documentElement;
 		const isPreviousDark = rootNode.classList.contains("dark");
-		rootNode.className = "kirakira";
 		if (`${isColoredSidebar}` === "true") rootNode.classList.add("colored-sidebar");
 		if (`${isSharpAppearanceMode}` === "true") rootNode.classList.add("sharp");
 		if (`${isFlatAppearanceMode}` === "true") rootNode.classList.add("flat");
-		if (themeColor && themeColor === CUSTOM_THEME_COLOR && customerThemeColor)
-			console.log("themeCustomerColor", customerThemeColor); // FIXME: "themeCustomColor" // TODO 设置自定义主题色
+		if (themeColor && themeColor === CUSTOM_THEME_COLOR && customThemeColor)
+			console.log("customThemeColor", customThemeColor); // TODO 设置自定义主题色
 		else if (themeColor)
 			rootNode.classList.add(themeColor);
 		if (currentThemeType) {
@@ -143,7 +142,7 @@ export function cookieBinding() {
 			const isDark = actualThemeType === "dark";
 			const updateThemeSettings = () => rootNode.classList.toggle("dark", isDark);
 
-			if (!lastClickMouseEvent) updateThemeSettings();
+			if (typeof lastClickMouseEvent === "undefined" || !lastClickMouseEvent) updateThemeSettings(); // 使用 typeof foo === "undefined" 方式判断 lastClickMouseEvent 是否被声明
 			else if (isPreviousDark !== isDark) {
 				if (isPreviousDark) rootNode.classList.add("dark");
 				const { x, y } = lastClickMouseEvent;
@@ -167,11 +166,11 @@ export function cookieBinding() {
 		}
 		// HACK 14 在此处添加
 	} catch (error) {
-		console.error("ERROR", "ERROR IN cookieBander", error);
+		console.error("ERROR", "ERROR IN cookieBinding", error);
 		try {
 			document.documentElement.classList.add(SYSTEM_THEME, THEME_LIGHT);
 		} catch (error) {
-			console.error("ERROR", "ERROR IN cookieBander -> catch", error);
+			console.error("ERROR", "ERROR IN cookieBinding -> catch", error);
 		}
 	}
 }
