a0d3086831d7a540161a98276a19f970f8167bb9
cfdxkk <b2567240058@gmail.com>
2024-03-18 22:52:25 +0800

    :bug: 修复无法查看封面的问题

--- components/Creation/CreationDetail.vue
+++ components/Creation/CreationDetail.vue
@@ -26,7 +26,7 @@
 	 */
 	function downloadCover() {
 		if (!props.cover) return;
-		window.open(`${props.cover}/w=999999`, "_blank"); // TODO: 暂时改为在新标签页中直接打开图片链接
+		window.open(`https://kirafile.com/cdn-cgi/imagedelivery/Gyz90amG54C4b_dtJiRpYg/${props.cover}/w=999999`, "_blank"); // TODO: 暂时改为在新标签页中直接打开图片链接
 		// downloadFile(props.cover, `${props.title} (kv${props.videoId})`);
 	}
 </script>
