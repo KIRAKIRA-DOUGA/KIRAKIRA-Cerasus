62876c447bae6d2e5099e0f68895a69ed57583c0
cfdxkk <b2567240058@gmail.com>
2024-03-21 22:48:50 +0800

    :lock: 用户密码在前端被 Hash 一次以提高安全性
    这个功能的唯一作用只是让一些有“安全洁癖”的用户放心：“即使是 KIRAKIRA 管理员，也无法查看你的密码”
    在前端被（不加盐） Hash 过的密码，到了后端还会再加盐 Hash 一次。
    
    注意：此次提交的同时清空了用户数据表。

---
+++ utils/hash-utils.ts
@@ -0,0 +1,16 @@
+/**
+ * 使用 crypto 异步生成一个不加盐的十六进制 SHA-256 Hash
+ * @param input 原字符串
+ * @returns Hash 结果
+ */
+export async function generateHash(input: string): Promise<string> {
+	const encoder = new TextEncoder();
+	const data = encoder.encode(input);
+
+	// 使用 SHA-256 算法生成哈希 // NOTE 只能在安全上下文中使用，例如：HTTPS, localhost, Web/Service Workers 或 浏览器扩展 等
+	const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+	// 将哈希值转换为 64 个字符的十六进制字符串
+	return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+}
