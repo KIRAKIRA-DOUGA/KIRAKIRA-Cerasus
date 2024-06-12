07e0e7cd65fa6927299dd006ef3a7a9acf56a487
otomad <shuishenme@outlook.com>
2024-06-13 03:29:35 +0800

    Add sleep function

--- utils/delay.ts
+++ utils/delay.ts
@@ -1,6 +1,6 @@
 /**
- * 没错，就是大名鼎鼎的延迟函数。
- * 叫 sleep 也行。
+ * 没错，就是大名鼎鼎的延迟函数。\
+ * 这将异步执行，不会阻塞线程。
  * @param ms - 毫秒值。
  * @returns 空返回值。
  */
@@ -8,10 +8,20 @@ export function delay(ms: number): Promise<void> {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const isb = new Int32Array(new SharedArrayBuffer(4));
+/**
+ * 睡眠函数。\
+ * 这将会阻塞线程。
+ * @param ms - 毫秒值。
+ */
+export function sleep(ms: number) {
+	Atomics.wait(isb, 0, 0, ms);
+}
+
 /**
  * 计划每延迟几毫秒重复执行一次回调。
  * @param callback - 计时器超时时要调用的回调函数。
- * @param ms 调用回调函数之前等待的毫秒数。
+ * @param ms - 调用回调函数之前等待的毫秒数。
  * @returns 用于取消计时器的 ID 引用。
  */
 export function createInterval(callback: () => void, ms: number) {
