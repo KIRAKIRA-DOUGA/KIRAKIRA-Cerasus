22dc2e9c6341bf4033dcf6a6a65109e474850127
Aira Sakuranomiya <aira@aira.cafe>
2024-08-09 22:10:19 +0800

    :zap: Use Google Fonts to reduce data transfer

--- app.vue
+++ app.vue
@@ -78,6 +78,9 @@
 			{ rel: "alternate", href: `${homepage}ko`, hreflang: "ko" },
 			{ rel: "alternate", href: `${homepage}vi`, hreflang: "vi" },
 			{ rel: "alternate", href: `${homepage}id`, hreflang: "id" },
+			{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+			{ rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+			{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" },
 		],
 	});
 
