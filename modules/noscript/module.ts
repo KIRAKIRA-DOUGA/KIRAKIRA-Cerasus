5956c8f61df39a9ca21a64fdb649250401c7ca4d
otomad <shuishenme@outlook.com>
2024-08-18 21:57:09 +0800

    Fix a typo

--- modules/noscript/module.ts
+++ modules/noscript/module.ts
@@ -1,14 +1,14 @@
 import { addServerHandler, addTemplate, defineNuxtModule } from "@nuxt/kit";
 import { PREFERENTIAL_TEMPLATE_PATH } from "../shared/constants";
 import { compileSassFile, createReadFileResolver, minifyHtml } from "../shared/encode";
-import { NOSCRIPT_HTML_FILE, NOSCRPIT_SCSS_FILE, NOSCRIPT_ROUTE } from "./constants";
+import { NOSCRIPT_HTML_FILE, NOSCRIPT_SCSS_FILE, NOSCRIPT_ROUTE } from "./constants";
 
 export default defineNuxtModule({
 	async setup(_options, nuxt) {
 		const { readFile, resolve } = createReadFileResolver(__dirname);
 
 		let noscriptHtml = await readFile(NOSCRIPT_HTML_FILE);
-		const noscriptStyle = compileSassFile(__dirname, NOSCRPIT_SCSS_FILE);
+		const noscriptStyle = compileSassFile(__dirname, NOSCRIPT_SCSS_FILE);
 		noscriptHtml = noscriptHtml.replace(/(?=\s*<\/head>)/, `\n<style>\n${noscriptStyle}\n</style>`);
 		noscriptHtml = await minifyHtml(noscriptHtml);
 		addTemplate({
@@ -16,12 +16,12 @@ export default defineNuxtModule({
 			write: true,
 			getContents: () => noscriptHtml,
 		});
-		
+
 		nuxt.hook("nitro:config", nitro => {
 			nitro.plugins ??= [];
 			nitro.plugins.push(resolve("nitro-plugin.ts"));
 		});
-		
+
 		addServerHandler({
 			route: NOSCRIPT_ROUTE,
 			handler: resolve("route"),
